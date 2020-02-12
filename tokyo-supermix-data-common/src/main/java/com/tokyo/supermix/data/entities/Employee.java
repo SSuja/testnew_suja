f72e149fe2e215ff7aa14409ab65dda63cf9dec2
geerthana65 <geerthana65@gmail.com>
2020-02-12 21:37:34 +0530

    Changed to Employee & Designation Relationship

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/Employee.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/Employee.java
@@ -1,14 +1,11 @@
 package com.tokyo.supermix.data.entities;
 
 import java.io.Serializable;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -16,89 +13,89 @@ import javax.persistence.Table;
 @Table(schema = "tokyo-supermix", name = "employee")
 public class Employee implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	private String firstName;
-	private String lastName;
-	private String email;
-	private String phoneNumber;
-	private String address;
-	@ManyToOne
-	@JoinColumn(name = "plantCode", nullable = false)
-	private Plant plant;
-	@ManyToMany
-	@JoinColumn(name = "designationId", nullable = false)
-	private List<Designation> designation;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String phoneNumber;
+  private String address;
+  @ManyToOne
+  @JoinColumn(name = "plantCode", nullable = false)
+  private Plant plant;
+  @ManyToOne
+  @JoinColumn(name = "designationId", nullable = false)
+  private Designation designation;
 
-	public Long getId() {
-		return id;
-	}
+  public Long getId() {
+    return id;
+  }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	public String getFirstName() {
-		return firstName;
-	}
+  public String getFirstName() {
+    return firstName;
+  }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
-	public String getLastName() {
-		return lastName;
-	}
+  public String getLastName() {
+    return lastName;
+  }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 
-	public String getEmail() {
-		return email;
-	}
+  public String getEmail() {
+    return email;
+  }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
 
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
 
-	public String getAddress() {
-		return address;
-	}
+  public String getAddress() {
+    return address;
+  }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+  public void setAddress(String address) {
+    this.address = address;
+  }
 
-	public Plant getPlant() {
-		return plant;
-	}
+  public Plant getPlant() {
+    return plant;
+  }
 
-	public void setPlant(Plant plant) {
-		this.plant = plant;
-	}
+  public void setPlant(Plant plant) {
+    this.plant = plant;
+  }
 
-	public List<Designation> getDesignation() {
-		return designation;
-	}
+  public Designation getDesignation() {
+    return designation;
+  }
 
-	public void setDesignation(List<Designation> designation) {
-		this.designation = designation;
-	}
+  public void setDesignation(Designation designation) {
+    this.designation = designation;
+  }
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
+  public static long getSerialversionuid() {
+    return serialVersionUID;
+  }
 
 }
