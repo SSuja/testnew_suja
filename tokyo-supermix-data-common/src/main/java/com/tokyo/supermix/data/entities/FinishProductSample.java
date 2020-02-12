c9abbf2b88923d63a4cbfb9368537de42c328504
geerthana65 <geerthana65@gmail.com>
2020-02-12 16:23:23 +0530

    Entity modification(FinishProductSample & Pour)

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/FinishProductSample.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/FinishProductSample.java
@@ -7,76 +7,88 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(schema = "tokyo-supermix", name = "finish_product_sample")
-public class FinishProductSample implements Serializable{
+public class FinishProductSample implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	@Id
-	private String code;
-	private Date dateAndTime;
-	private String sampleSize;
-	private String workOrderNo;
-	@ManyToOne
-	@JoinColumn(name = "mixDesignCode", nullable = false)
-	private MixDesign mixDesign;
-	@ManyToOne
-	@JoinColumn(name = "projectCode", nullable = false)
-	private Project project;
+  @Id
+  private String code;
+  private Date dateAndTime;
+  private String sampleSize;
+  private String workOrderNo;
+  @ManyToOne
+  @JoinColumn(name = "mixDesignCode", nullable = false)
+  private MixDesign mixDesign;
+  @ManyToOne
+  @JoinColumn(name = "projectCode", nullable = false)
+  private Project project;
+  @OneToOne
+  @JoinColumn(name = "pourId", nullable = false)
+  private Pour pour;
 
-	public String getCode() {
-		return code;
-	}
+  public String getCode() {
+    return code;
+  }
 
-	public void setCode(String code) {
-		this.code = code;
-	}
+  public void setCode(String code) {
+    this.code = code;
+  }
 
-	public Date getDateAndTime() {
-		return dateAndTime;
-	}
+  public Date getDateAndTime() {
+    return dateAndTime;
+  }
 
-	public void setDateAndTime(Date dateAndTime) {
-		this.dateAndTime = dateAndTime;
-	}
+  public void setDateAndTime(Date dateAndTime) {
+    this.dateAndTime = dateAndTime;
+  }
 
-	public String getSampleSize() {
-		return sampleSize;
-	}
+  public String getSampleSize() {
+    return sampleSize;
+  }
 
-	public void setSampleSize(String sampleSize) {
-		this.sampleSize = sampleSize;
-	}
+  public void setSampleSize(String sampleSize) {
+    this.sampleSize = sampleSize;
+  }
 
-	public String getWorkOrderNo() {
-		return workOrderNo;
-	}
+  public String getWorkOrderNo() {
+    return workOrderNo;
+  }
 
-	public void setWorkOrderNo(String workOrderNo) {
-		this.workOrderNo = workOrderNo;
-	}
+  public void setWorkOrderNo(String workOrderNo) {
+    this.workOrderNo = workOrderNo;
+  }
 
-	public MixDesign getMixDesign() {
-		return mixDesign;
-	}
+  public MixDesign getMixDesign() {
+    return mixDesign;
+  }
 
-	public void setMixDesign(MixDesign mixDesign) {
-		this.mixDesign = mixDesign;
-	}
+  public void setMixDesign(MixDesign mixDesign) {
+    this.mixDesign = mixDesign;
+  }
 
-	public Project getProject() {
-		return project;
-	}
+  public Project getProject() {
+    return project;
+  }
 
-	public void setProject(Project project) {
-		this.project = project;
-	}
+  public void setProject(Project project) {
+    this.project = project;
+  }
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
+  public static long getSerialversionuid() {
+    return serialVersionUID;
+  }
+
+  public Pour getPour() {
+    return pour;
+  }
+
+  public void setPour(Pour pour) {
+    this.pour = pour;
+  }
 
 }
--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/Pour.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/Pour.java
@@ -7,67 +7,67 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(schema = "tokyo-supermix", name = "pour")
 public class Pour implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	private String name;
-	private String number;
-	private String description;
-	@OneToOne
-	@JoinColumn(name = "finishProductSampleCode", nullable = false)
-	private FinishProductSample finishProductSample;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String name;
+  private String number;
+  private String description;
+  @ManyToOne
+  @JoinColumn(name = "projectCode", nullable = false)
+  private Project project;
 
-	public Long getId() {
-		return id;
-	}
+  public Long getId() {
+    return id;
+  }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public String getNumber() {
-		return number;
-	}
+  public String getNumber() {
+    return number;
+  }
 
-	public void setNumber(String number) {
-		this.number = number;
-	}
+  public void setNumber(String number) {
+    this.number = number;
+  }
 
-	public String getDescription() {
-		return description;
-	}
+  public String getDescription() {
+    return description;
+  }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-	public FinishProductSample getFinishProductSample() {
-		return finishProductSample;
-	}
+  public Project getProject() {
+    return project;
+  }
 
-	public void setFinishProductSample(FinishProductSample finishProductSample) {
-		this.finishProductSample = finishProductSample;
-	}
+  public void setProject(Project project) {
+    this.project = project;
+  }
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
+  public static long getSerialversionuid() {
+    return serialVersionUID;
+  }
 
 }
