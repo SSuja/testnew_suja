51c4ad07c702820439ac94adfe5ff314a4357aad
SSuja <sujamathy@gmail.com>
2020-02-12 15:57:01 +0530

    update target to handler and Fixed add plantAPI

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/PlantController.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/PlantController.java
@@ -15,6 +15,7 @@ import com.tokyo.supermix.data.mapper.Mapper;
 import com.tokyo.supermix.rest.enums.RestApiResponseStatus;
 import com.tokyo.supermix.rest.response.ApiResponse;
 import com.tokyo.supermix.rest.response.BasicResponse;
+import com.tokyo.supermix.rest.response.ContentResponse;
 import com.tokyo.supermix.rest.response.ValidationFailure;
 import com.tokyo.supermix.server.services.PlantService;
 import com.tokyo.supermix.util.Constants;
@@ -41,13 +42,13 @@ public class PlantController {
 	public ResponseEntity<Object> createPlant(@RequestBody PlantDto plantDto) {
 		if (plantService.isPlantAlreadyExist(plantDto.getName())) {
 			logger.debug("Plant already exists: createPlant(), plantName: {}");
-				 return new ResponseEntity<>(new BasicResponse<>(
+				 return new ResponseEntity<>(new ContentResponse<>("",
 			          new ValidationFailure(Constants.PLANT_NAME, validationFailureStatusCodes.getPlantAlreadyExist()),
-			          RestApiResponseStatus.VALIDATION_FAILURE,ValidationConstance.PLANT_EXIST), HttpStatus.BAD_REQUEST);
+			          RestApiResponseStatus.VALIDATION_FAILURE), HttpStatus.BAD_REQUEST);
 		}
 		Plant plant = mapper.map(plantDto, Plant.class);
 		plantService.createPlant(plant);
-		return new ResponseEntity<>(new ApiResponse(RestApiResponseStatus.OK,Constants.ADD_PLANT_SUCCESS), HttpStatus.OK);
+		return new ResponseEntity<>(new BasicResponse<>(RestApiResponseStatus.OK,Constants.ADD_PLANT_SUCCESS), HttpStatus.OK);
 
 	}
 
