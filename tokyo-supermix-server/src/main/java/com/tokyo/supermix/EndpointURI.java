80953d7ebaa02ff6a31991237b1df9d5d56703cf
Mathuragini <mathuthiyasharma@gmail.com>
2020-02-13 13:52:48 +0530

    add designation api resolve review

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/EndpointURI.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/EndpointURI.java
@@ -13,6 +13,7 @@ public final class EndpointURI {
 
 	private static final String SLASH = "/";
 	private static final String CODE = "/{code}";
+	private static final String ID= "/{id}";
 	private static final String SEARCH = "/search";
 
 	/*
@@ -26,8 +27,8 @@ public final class EndpointURI {
 	 * Designation API
 	 */
 	public static final String DESIGNATION = BASE_API_PATH + "designation";
-	public static final String UPDATE_DESIGNATION = DESIGNATION + CODE;
-	public static final String GET_DESIGNATION_BY_CODE = DESIGNATION + CODE;
+	public static final String UPDATE_DESIGNATION = DESIGNATION + ID;
+	public static final String GET_DESIGNATION_BY_CODE = DESIGNATION + ID;
 
 	private EndpointURI() {
 	}
