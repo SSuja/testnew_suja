fb79422b3df4490828fd7b0c7593bf3afdea906f
SSuja <sujamathy@gmail.com>
2020-02-13 10:31:27 +0530

    remove line space and unused import in plant controller

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/PlantController.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/PlantController.java
@@ -7,29 +7,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.tokyo.supermix.EndpointURI;
 import com.tokyo.supermix.data.dto.PlantDto;
 import com.tokyo.supermix.data.entities.Plant;
 import com.tokyo.supermix.data.mapper.Mapper;
 import com.tokyo.supermix.rest.enums.RestApiResponseStatus;
-import com.tokyo.supermix.rest.response.ApiResponse;
 import com.tokyo.supermix.rest.response.BasicResponse;
 import com.tokyo.supermix.rest.response.ContentResponse;
 import com.tokyo.supermix.rest.response.ValidationFailure;
 import com.tokyo.supermix.server.services.PlantService;
 import com.tokyo.supermix.util.Constants;
-import com.tokyo.supermix.util.ValidationConstance;
 import com.tokyo.supermix.util.ValidationFailureStatusCodes;
 
 @RestController
 public class PlantController {
 	@Autowired
 	PlantService plantService;
-
-
 	
-
 	@Autowired
 	ValidationFailureStatusCodes validationFailureStatusCodes;
 
