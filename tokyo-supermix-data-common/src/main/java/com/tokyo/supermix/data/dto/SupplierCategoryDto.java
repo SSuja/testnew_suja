9fa0168e191733e4775067b9c6b633836633ccba
geerthana65 <geerthana65@gmail.com>
2020-02-13 20:34:24 +0530

    NotNull Validation

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/dto/SupplierCategoryDto.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/dto/SupplierCategoryDto.java
@@ -1,38 +1,38 @@
 package com.tokyo.supermix.data.dto;
 
+import javax.persistence.Column;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 
 public class SupplierCategoryDto {
 
-	private Long id;
-	@NotNull
-	private String category;
-	@Null
-	private String description;
+  private Long id;
+  @NotNull(message = "{supplierCategoryDto.category.blank}")
+  private String category;
+  @Column(nullable = true)
+  private String description;
 
-	public Long getId() {
-		return id;
-	}
+  public Long getId() {
+    return id;
+  }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	public String getCategory() {
-		return category;
-	}
+  public String getCategory() {
+    return category;
+  }
 
-	public void setCategory(String category) {
-		this.category = category;
-	}
+  public void setCategory(String category) {
+    this.category = category;
+  }
 
-	public String getDescription() {
-		return description;
-	}
+  public String getDescription() {
+    return description;
+  }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
 }
--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/util/Constants.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/util/Constants.java
@@ -1,22 +1,24 @@
 package com.tokyo.supermix.util;
 
 public class Constants {
-	public static final String PLANT_NAME = "Plant Name";
+  public static final String PLANT_NAME = "Plant Name";
 
-	// Success Messages
-	public static final String ADD_PLANT_SUCCESS = "Plant added Successfully";
+  // Success Messages
+  public static final String ADD_PLANT_SUCCESS = "Plant added Successfully";
 
-	/*
-	 * Supplier Category
-	 */
-	public static final String SUPPLIER_CATEGORY_NAME = "Supplier Category";
+  /*
+   * Supplier Category
+   */
+  public static final String SUPPLIER_CATEGORY_NAME = "Supplier Category";
 
-	/*
-	 * Success Messages for Supplier Category
-	 */
-	public static final String ADD_SUPPLIER_CATEGORY_SUCCESS = "Supplier Category added Successfully";
+  /*
+   * Success Messages for Supplier Category
+   */
+  public static final String ADD_SUPPLIER_CATEGORY_SUCCESS = "Supplier Category added Successfully";
 
-	// encaptulate constructor to restrict modification from outside
-	private Constants() {
-	}
+  // encaptulate constructor to restrict modification from outside
+  private Constants() {}
+
+  // constants for validation key
+  public static final String SUPPLIER_CATEGORY = "supplier-category";
 }
