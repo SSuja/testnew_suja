d6a21c374d1460a75e0f7ddaeb6033ce264b6330
SSuja <sujamathy@gmail.com>
2020-02-13 15:33:50 +0530

    add javax.validation dependancy and add null validation to plant

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/util/Constants.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/util/Constants.java
@@ -7,6 +7,12 @@ public class Constants {
 	//Success Messages
 	public static final String ADD_PLANT_SUCCESS = "Plant added Successfully";
 
+	
+	//constants for validation key
+	public static final String PLANT = "plant";
+	
+	
+	
 	//encaptulate constructor to restrict modification from outside
 	private Constants() {
 	}
