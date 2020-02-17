5ebf88fc92658f16e8ea1bd4ba0339113c683ed4
Mathuragini <mathuthiyasharma@gmail.com>
2020-02-17 13:08:22 +0530

    delete-designation-by-id-conflict-resolve

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/util/ValidationFailureStatusCodes.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/util/ValidationFailureStatusCodes.java
@@ -12,64 +12,40 @@ import org.springframework.stereotype.Component;
 @Component
 @PropertySource("classpath:ValidationMessages.properties")
 public class ValidationFailureStatusCodes {
-  /*
-   * Plant
-   */
-  @Value("${validation.plant.notExists}")
-  private String plantNotExist;
+	/*
+	 * Plant
+	 */
+	@Value("${validation.plant.notExists}")
+	private String plantNotExist;
 
-  @Value("${validation.plant.alreadyExist}")
-  private String plantAlreadyExist;
+	@Value("${validation.plant.alreadyExist}")
+	private String plantAlreadyExist;
 
-  @Value("${validation.supplierCategory.alreadyExist}")
-  private String supplierCategoryAlreadyExist;
+	@Value("${validation.supplierCategory.alreadyExist}")
+	private String supplierCategoryAlreadyExist;
 
-  public String getPlantNotExist() {
-    return plantNotExist;
-  }
-
-  public void setPlantNotExist(String plantNotExist) {
-    this.plantNotExist = plantNotExist;
-  }
-
-  public String getPlantAlreadyExist() {
-    return plantAlreadyExist;
-  }
-
-  public void setPlantAlreadyExist(String plantAlreadyExist) {
-    this.plantAlreadyExist = plantAlreadyExist;
-  }
-
-  public String getSupplierCategoryAlreadyExist() {
-    return supplierCategoryAlreadyExist;
-  }
-
-  public void setSupplierCategoryAlreadyExist(String supplierCategoryAlreadyExist) {
-    this.supplierCategoryAlreadyExist = supplierCategoryAlreadyExist;
-  }
-
-	// for designation
-
-	@Value("${validation.designation.notExists}")
-	private String designationNotExist;
-
-	@Value("${validation.designation.alreadyExist}")
-	private String designationAlreadyExist;
-
-	public String getDesignationNotExist() {
-		return designationNotExist;
+	public String getPlantNotExist() {
+		return plantNotExist;
 	}
 
-	public void setDesignationNotExist(String designationNotExist) {
-		this.designationNotExist = designationNotExist;
+	public void setPlantNotExist(String plantNotExist) {
+		this.plantNotExist = plantNotExist;
 	}
 
-	public String getDesignationAlreadyExist() {
-		return designationAlreadyExist;
+	public String getPlantAlreadyExist() {
+		return plantAlreadyExist;
 	}
 
-	public void setDesignationAlreadyExist(String designationAlreadyExist) {
-		this.designationAlreadyExist = designationAlreadyExist;
+	public void setPlantAlreadyExist(String plantAlreadyExist) {
+		this.plantAlreadyExist = plantAlreadyExist;
+	}
+
+	public String getSupplierCategoryAlreadyExist() {
+		return supplierCategoryAlreadyExist;
+	}
+
+	public void setSupplierCategoryAlreadyExist(String supplierCategoryAlreadyExist) {
+		this.supplierCategoryAlreadyExist = supplierCategoryAlreadyExist;
 	}
 
 	// for designation
