499acb2f833bf3c0ea9135227a2a9206cdef44a4
jananthan93 <jjananthan93@gmail.com>
2020-02-20 08:52:15 +0530

    missing supplier dto added

---
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/dto/SupplierRequestDto.java
@@ -0,0 +1,75 @@
+package com.tokyo.supermix.data.dto;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class SupplierRequestDto {
+  private Long id;
+  @NotNull(message = "{supplierDto.name.null}")
+  @NotEmpty(message = "{supplierDto.name.empty}")
+  private String name;
+  private String companyName;
+  private String address;
+  private String phoneNumber;
+  @NotNull(message = "{supplierDto.name.null}")
+  @NotEmpty(message = "{supplierDto.name.empty}")
+  private String email;
+  private Long suppilerCategoryId;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCompanyName() {
+    return companyName;
+  }
+
+  public void setCompanyName(String companyName) {
+    this.companyName = companyName;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Long getSuppilerCategoryId() {
+    return suppilerCategoryId;
+  }
+
+  public void setSuppilerCategoryId(Long suppilerCategoryId) {
+    this.suppilerCategoryId = suppilerCategoryId;
+  }
+
+}
---
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/dto/SupplierResponseDto.java
@@ -0,0 +1,70 @@
+package com.tokyo.supermix.data.dto;
+
+public class SupplierResponseDto
+{
+	private Long id;
+	private String name;
+	private String companyName;
+	private String address;
+	private String phoneNumber;
+	private String email;
+	private String suppilerCategory;
+	public Long getId()
+	{
+		return id;
+	}
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public String getCompanyName()
+	{
+		return companyName;
+	}
+	public void setCompanyName(String companyName)
+	{
+		this.companyName = companyName;
+	}
+	public String getAddress()
+	{
+		return address;
+	}
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+	public String getPhoneNumber()
+	{
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber)
+	{
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail()
+	{
+		return email;
+	}
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+	public String getSuppilerCategory()
+	{
+		return suppilerCategory;
+	}
+	public void setSuppilerCategory(String suppilerCategory)
+	{
+		this.suppilerCategory = suppilerCategory;
+	}
+	
+	
+}
