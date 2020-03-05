76743356106cb694c56f1d2600bcfc39657273bf
jananthan93 <jjananthan93@gmail.com>
2020-03-05 16:55:13 +0530

    change

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/TokyoSupermixServer.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/TokyoSupermixServer.java
@@ -1,15 +1,13 @@
 package com.tokyo.supermix;
 
-import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TokyoSupermixServer {
 
-	public static void main(String[] args) {
-	  BasicConfigurator.configure();
-		SpringApplication.run(TokyoSupermixServer.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TokyoSupermixServer.class, args);
+    }
 
 }
