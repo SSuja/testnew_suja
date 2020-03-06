43d016160d648ed542afa251c057500172ba493a
kirikaran <kirikarankiri63@gmail.com>
2020-03-06 16:59:19 +0530

    Add service implementation and methods

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/AcceptedValueService.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/AcceptedValueService.java
@@ -1,5 +1,18 @@
 package com.tokyo.supermix.server.services;
 
+import java.util.List;
+import com.tokyo.supermix.data.entities.AcceptedValue;
+
 public interface AcceptedValueService {
+
+  public void saveAcceptedValue(AcceptedValue acceptedValue);
+
+  public List<AcceptedValue> getAllAcceptedValues();
+
+  boolean isAcceptedValueExist(Long id);
+
+  public AcceptedValue getAcceptedValueById(Long id);
+
+  public void deleteAcceptedValue(Long id);
 
 }
--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/AcceptedValueServiceImpl.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/AcceptedValueServiceImpl.java
@@ -1,8 +1,42 @@
 package com.tokyo.supermix.server.services;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import com.tokyo.supermix.data.entities.AcceptedValue;
+import com.tokyo.supermix.data.repositories.AcceptedValueRepository;
 
 @Service
 public class AcceptedValueServiceImpl implements AcceptedValueService {
+
+  @Autowired
+  private AcceptedValueRepository acceptedValueRepository;
+
+  @Transactional
+  public void saveAcceptedValue(AcceptedValue acceptedValue) {
+    acceptedValueRepository.save(acceptedValue);
+  }
+
+  @Transactional(readOnly = true)
+  public List<AcceptedValue> getAllAcceptedValues() {
+    return acceptedValueRepository.findAll();
+  }
+
+  @Transactional(readOnly = true)
+  public boolean isAcceptedValueExist(Long id) {
+    return acceptedValueRepository.existsById(id);
+  }
+
+  @Transactional(readOnly = true)
+  public AcceptedValue getAcceptedValueById(Long id) {
+    return acceptedValueRepository.findById(id).get();
+  }
+
+  @Transactional(propagation = Propagation.NEVER)
+  public void deleteAcceptedValue(Long id) {
+    acceptedValueRepository.deleteById(id);
+  }
 
 }
