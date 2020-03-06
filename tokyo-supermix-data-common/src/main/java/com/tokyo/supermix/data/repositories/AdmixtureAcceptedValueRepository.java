10f0897ced81a05199a347c3be44ba39d35c3cca
kirikaran <kirikarankiri63@gmail.com>
2020-03-06 16:06:27 +0530

    create acceptedValue and admixtureAcceptedValue all files

---
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/AdmixtureAcceptedValueRepository.java
@@ -0,0 +1,8 @@
+package com.tokyo.supermix.data.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tokyo.supermix.data.entities.AdmixtureAcceptedValue;
+
+public interface AdmixtureAcceptedValueRepository
+    extends JpaRepository<AdmixtureAcceptedValue, Long> {
+}
