0abcce1e5a746cababa7016f59c294b5ba5f3018
kirikaran <kirikarankiri63@gmail.com>
2020-03-31 21:15:59 +0530

    conflic resolved

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/MaterialTestTrialServiceImpl.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/MaterialTestTrialServiceImpl.java
@@ -20,123 +20,114 @@ import com.tokyo.supermix.util.MailConstants;
 
 @Service
 public class MaterialTestTrialServiceImpl implements MaterialTestTrialService {
-  @Autowired
-  private EmailService emailService;
-  @Autowired
-  private MailConstants mailConstants;
-  @Autowired
-  private MaterialTestTrialRepository materialTestTrialRepository;
-  @Autowired
-  private AcceptedValueRepository acceptedValueRepository;
-  @Autowired
-  private MaterialTestRepository materialTestRepository;
-  @Autowired
-  private AdmixtureAcceptedValueRepository admixtureAcceptedValueRepository;
+	@Autowired
+	private EmailService emailService;
+	@Autowired
+	private MailConstants mailConstants;
+	@Autowired
+	private MaterialTestTrialRepository materialTestTrialRepository;
+	@Autowired
+	private AcceptedValueRepository acceptedValueRepository;
+	@Autowired
+	private MaterialTestRepository materialTestRepository;
+	@Autowired
+	private AdmixtureAcceptedValueRepository admixtureAcceptedValueRepository;
 
-  @Transactional
-  public MaterialTestTrial saveMaterialTestTrial(MaterialTestTrial materialTestTrial) {
-    return materialTestTrialRepository.save(materialTestTrial);
-  }
+	@Transactional
+	public MaterialTestTrial saveMaterialTestTrial(MaterialTestTrial materialTestTrial) {
+		return materialTestTrialRepository.save(materialTestTrial);
+	}
 
-  @Transactional(readOnly = true)
-  public List<MaterialTestTrial> getAllMaterialTestTrial() {
-    return materialTestTrialRepository.findAll();
-  }
+	@Transactional(readOnly = true)
+	public List<MaterialTestTrial> getAllMaterialTestTrial() {
+		return materialTestTrialRepository.findAll();
+	}
 
-  @Transactional(readOnly = true)
-  public MaterialTestTrial getMaterialTestTrialByCode(String code) {
-    return materialTestTrialRepository.findByCode(code);
-  }
+	@Transactional(readOnly = true)
+	public MaterialTestTrial getMaterialTestTrialByCode(String code) {
+		return materialTestTrialRepository.findByCode(code);
+	}
 
-  @Transactional(propagation = Propagation.NEVER)
-  public void deleteMaterialTestTrial(String code) {
-    materialTestTrialRepository.deleteById(code);
-  }
+	@Transactional(propagation = Propagation.NEVER)
+	public void deleteMaterialTestTrial(String code) {
+		materialTestTrialRepository.deleteById(code);
+	}
 
-  @Transactional(readOnly = true)
-  public boolean isMaterialTestTrialExits(String code) {
-    return materialTestTrialRepository.existsByCode(code);
-  }
+	@Transactional(readOnly = true)
+	public boolean isMaterialTestTrialExits(String code) {
+		return materialTestTrialRepository.existsByCode(code);
+	}
 
-  @Transactional(readOnly = true)
-  public List<MaterialTestTrial> getMaterialTestTrialByMaterialTestCode(String materialTestCode) {
-    return materialTestTrialRepository.findByMaterialTestCode(materialTestCode);
-  }
+	@Transactional(readOnly = true)
+	public List<MaterialTestTrial> getMaterialTestTrialByMaterialTestCode(String materialTestCode) {
+		return materialTestTrialRepository.findByMaterialTestCode(materialTestCode);
+	}
 
-  @Transactional(readOnly = true)
-  public boolean isMaterialTestIdExits(String materialTestCode) {
-    return materialTestTrialRepository.existsByMaterialTestCode(materialTestCode);
-  }
+	@Transactional(readOnly = true)
+	public boolean isMaterialTestIdExits(String materialTestCode) {
+		return materialTestTrialRepository.existsByMaterialTestCode(materialTestCode);
+	}
 
-  @Transactional
-  public void getAverageAndStatus(String materialTestCode) {
-    compareWithAverage(calculateAverage(materialTestCode), materialTestCode);
-    MaterialTest materialTest = materialTestRepository.findByCode(materialTestCode);
-    emailService.sendMailWithFormat(mailConstants.getMailUpdateMaterialTestStatus(),
-        Constants.SUBJECT_NEW_MATERIAL_TEST,
-        "<p>The Incoming Sample Code is <b>" + materialTest.getIncomingSample().getCode()
-            + ".</b> for Material test. The test is <b>" + materialTest.getStatus()
-            + "</b>.</p> <p>Test Details : </p><ul>" + "<li>Test Name : "
-            + materialTest.getTest().getName() + "</li><li> Average : " + materialTest.getAverage()
-            + "</li><li>Material : " + materialTest.getIncomingSample().getRawMaterial().getName()
-            + "</li><li> Date : " + materialTest.getDate() + "</li></ul>");
-  }
+	@Transactional
+	public void getAverageAndStatus(String materialTestCode) {
+		compareWithAverage(calculateAverage(materialTestCode), materialTestCode);
+		MaterialTest materialTest = materialTestRepository.findByCode(materialTestCode);
+		emailService.sendMailWithFormat(mailConstants.getMailUpdateMaterialTestStatus(),
+				Constants.SUBJECT_NEW_MATERIAL_TEST,
+				"<p>The Incoming Sample Code is <b>" + materialTest.getIncomingSample().getCode()
+						+ ".</b> for Material test. The test is <b>" + materialTest.getStatus()
+						+ "</b>.</p> <p>Test Details : </p><ul>" + "<li>Test Name : " + materialTest.getTest().getName()
+						+ "</li><li> Average : " + materialTest.getAverage() + "</li><li>Material : "
+						+ materialTest.getIncomingSample().getRawMaterial().getName() + "</li><li> Date : "
+						+ materialTest.getDate() + "</li></ul>");
+	}
 
+	private void compareWithAverage(Double average, String materialTestCode) {
+		MaterialTest materialTest = materialTestRepository.findByCode(materialTestCode);
+		Long testId = materialTest.getTest().getId();
+		if (materialTest.getIncomingSample().getRawMaterial().getMaterialSubCategory().getMaterialCategory().getName()
+				.equalsIgnoreCase(Constants.ADMIXTURE)) {
+			AdmixtureAcceptedValue admixtureAcceptedValue = admixtureAcceptedValueRepository
+					.findByTestIdAndRawMaterialId(testId, materialTest.getIncomingSample().getRawMaterial().getId());
+			calculateStatus(average, admixtureAcceptedValue.getMinValue(), admixtureAcceptedValue.getMaxValue(),
+					materialTestCode);
+		} else {
+			calculateStatus(average, acceptedValueRepository.findByTestId(testId).getMinValue(),
+					acceptedValueRepository.findByTestId(testId).getMaxValue(), materialTestCode);
+		}
+	}
 
-  private void compareWithAverage(Double average, String materialTestCode) {
-    MaterialTest materialTest = materialTestRepository.findByCode(materialTestCode);
-    Long testId = materialTest.getTest().getId();
-    if (materialTest.getIncomingSample().getRawMaterial().getMaterialSubCategory()
-        .getMaterialCategory().getName().equalsIgnoreCase(Constants.ADMIXTURE)) {
-      AdmixtureAcceptedValue admixtureAcceptedValue =
-          admixtureAcceptedValueRepository.findByTestIdAndRawMaterialId(testId,
-              materialTest.getIncomingSample().getRawMaterial().getId());
-      calculateStatus(average, admixtureAcceptedValue.getMinValue(),
-          admixtureAcceptedValue.getMaxValue(), materialTestCode);
-    } else {
-      calculateStatus(average, acceptedValueRepository.findByTestId(testId).getMinValue(),
-          acceptedValueRepository.findByTestId(testId).getMaxValue(), materialTestCode);
-    }
-  }
+	private void calculateStatus(Double average, Double minValue, Double maxValue, String materialTestCode) {
+		if (minValue <= average && average <= maxValue) {
+			updateAverage(average, materialTestCode, Status.PASS);
+		} else {
+			updateAverage(average, materialTestCode, Status.FAIL);
+		}
 
-  private void calculateStatus(Double average, Double minValue, Double maxValue,
-      String materialTestCode) {
-    if (minValue <= average && average <= maxValue) {
-      updateAverage(average, materialTestCode, Status.PASS);
-    } else {
-      updateAverage(average, materialTestCode, Status.FAIL);
-    }
+	}
 
-  }
+	private Double roundDoubleValue(Double value) {
+		DecimalFormat decimalFormat = new DecimalFormat(Constants.DECIMAL_FORMAT);
+		return Double.valueOf(decimalFormat.format(value));
+	}
 
-  private Double roundDoubleValue(Double value) {
-    DecimalFormat decimalFormat = new DecimalFormat(Constants.DECIMAL_FORMAT);
-    return Double.valueOf(decimalFormat.format(value));
-  }
+	private Double calculateAverage(String materialTestCode) {
+		Double totalResult = 0.0;
+		int trialTotal = 0;
+		List<MaterialTestTrial> listMaterialTestTrial = materialTestTrialRepository
+				.findByMaterialTestCode(materialTestCode);
+		for (MaterialTestTrial materialTestTrial : listMaterialTestTrial) {
+			totalResult = totalResult + materialTestTrial.getResult();
+			trialTotal = listMaterialTestTrial.size();
+		}
+		return roundDoubleValue(totalResult / trialTotal);
+	}
 
-  private Double calculateAverage(String materialTestCode) {
-    Double totalResult = 0.0;
-    int trialTotal = 0;
-    List<MaterialTestTrial> listMaterialTestTrial =
-        materialTestTrialRepository.findByMaterialTestCode(materialTestCode);
-    for (MaterialTestTrial materialTestTrial : listMaterialTestTrial) {
-      totalResult = totalResult + materialTestTrial.getResult();
-      trialTotal = listMaterialTestTrial.size();
-    }
-    return roundDoubleValue(totalResult / trialTotal);
-  }
-
-  @Transactional
-  public MaterialTest updateAverage(Double average, String code, Status status) {
-    MaterialTest materialTest = materialTestRepository.findByCode(code);
-    materialTest.setAverage(roundDoubleValue(average));
-    materialTest.setStatus(status);
-    return materialTestRepository.save(materialTest);
-  }
-
-  // To Change Four Digit Value
-  private Double roundDoubleValue(Double value) {
-    DecimalFormat decimalFormat = new DecimalFormat(Constants.DECIMAL_FORMAT);
-    return Double.valueOf(decimalFormat.format(value));
-  }
+	@Transactional
+	public MaterialTest updateAverage(Double average, String code, Status status) {
+		MaterialTest materialTest = materialTestRepository.findByCode(code);
+		materialTest.setAverage(roundDoubleValue(average));
+		materialTest.setStatus(status);
+		return materialTestRepository.save(materialTest);
+	}
 }
