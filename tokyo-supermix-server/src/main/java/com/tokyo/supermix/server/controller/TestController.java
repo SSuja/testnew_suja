8f7ae9b1771d0fdce2e6ece581017fa30f16484c
geerthana65 <geerthana65@gmail.com>
2020-04-22 11:30:02 +0530

    defect fixed - update test api

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/TestController.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/TestController.java
@@ -31,80 +31,81 @@ import com.tokyo.supermix.util.ValidationFailureStatusCodes;
 @CrossOrigin(origins = "*")
 public class TestController {
 
-	@Autowired
-	TestService testService;
+  @Autowired
+  private TestService testService;
 
-	@Autowired
-	ValidationFailureStatusCodes validationFailureStatusCodes;
+  @Autowired
+  private ValidationFailureStatusCodes validationFailureStatusCodes;
 
-	@Autowired
-	Mapper mapper;
+  @Autowired
+  private Mapper mapper;
 
-	private static final Logger logger = Logger.getLogger(UnitController.class);
+  private static final Logger logger = Logger.getLogger(TestController.class);
 
-	// create test api
-	@PostMapping(value = EndpointURI.TEST)
-	public ResponseEntity<Object> createTest(@Valid @RequestBody TestDto testDto) {
-		if (testService.isTestExist(testDto.getName())) {
-			logger.debug("Test already exists: createTest(), test: {}");
-			return new ResponseEntity<>(
-					new ValidationFailureResponse(Constants.TEST, validationFailureStatusCodes.getTestAlreadyExist()),
-					HttpStatus.BAD_REQUEST);
-		}
-		testService.saveTest(mapper.map(testDto, Test.class));
-		return new ResponseEntity<>(new BasicResponse<>(RestApiResponseStatus.OK, Constants.ADD_TEST_SUCCESS),
-				HttpStatus.OK);
-	}
+  // Create Test API
+  @PostMapping(value = EndpointURI.TEST)
+  public ResponseEntity<Object> createTest(@Valid @RequestBody TestDto testDto) {
+    if (testService.isTestExist(testDto.getName())) {
+      logger.debug("Test already exists: createTest(), test: {}");
+      return new ResponseEntity<>(new ValidationFailureResponse(Constants.TEST,
+          validationFailureStatusCodes.getTestAlreadyExist()), HttpStatus.BAD_REQUEST);
+    }
+    testService.saveTest(mapper.map(testDto, Test.class));
+    return new ResponseEntity<>(
+        new BasicResponse<>(RestApiResponseStatus.OK, Constants.ADD_TEST_SUCCESS), HttpStatus.OK);
+  }
 
-	// get all tests api
-	@GetMapping(value = EndpointURI.TESTS)
-	public ResponseEntity<Object> getAllTests() {
-		return new ResponseEntity<>(new ContentResponse<>(Constants.TEST,
-				mapper.map(testService.getAllTests(), TestDto.class), RestApiResponseStatus.OK), null, HttpStatus.OK);
-	}
+  // Get all Test API
+  @GetMapping(value = EndpointURI.TESTS)
+  public ResponseEntity<Object> getAllTests() {
+    return new ResponseEntity<>(new ContentResponse<>(Constants.TEST,
+        mapper.map(testService.getAllTests(), TestDto.class), RestApiResponseStatus.OK), null,
+        HttpStatus.OK);
+  }
 
-	// delete test api
-	@DeleteMapping(value = EndpointURI.GET_TEST_BY_ID)
-	public ResponseEntity<Object> deleteTest(@PathVariable Long id) {
-		if (testService.isTestExist(id)) {
-			testService.deleteTest(id);
-			return new ResponseEntity<>(new BasicResponse<>(RestApiResponseStatus.OK, Constants.DELETE_TEST_SCCESS),
-					HttpStatus.OK);
-		}
-		logger.debug("Invalid Id");
-		return new ResponseEntity<>(
-				new ValidationFailureResponse(Constants.TEST, validationFailureStatusCodes.getTestNotExist()),
-				HttpStatus.BAD_REQUEST);
-	}
+  // Delete Test API
+  @DeleteMapping(value = EndpointURI.GET_TEST_BY_ID)
+  public ResponseEntity<Object> deleteTest(@PathVariable Long id) {
+    if (testService.isTestExist(id)) {
+      testService.deleteTest(id);
+      return new ResponseEntity<>(
+          new BasicResponse<>(RestApiResponseStatus.OK, Constants.DELETE_TEST_SCCESS),
+          HttpStatus.OK);
+    }
+    logger.debug("Invalid Id");
+    return new ResponseEntity<>(new ValidationFailureResponse(Constants.TEST,
+        validationFailureStatusCodes.getTestNotExist()), HttpStatus.BAD_REQUEST);
+  }
 
-	// get test by id api
-	@GetMapping(value = EndpointURI.GET_TEST_BY_ID)
-	public ResponseEntity<Object> getTestById(@PathVariable Long id) {
-		if (testService.isTestExist(id)) {
-			logger.debug("Id found");
-			return new ResponseEntity<>(new ContentResponse<>(Constants.TEST,
-					mapper.map(testService.getTestById(id), TestDto.class), RestApiResponseStatus.OK), HttpStatus.OK);
-		}
-		logger.debug("Invalid id");
-		return new ResponseEntity<>(
-				new ValidationFailureResponse(Constants.TEST, validationFailureStatusCodes.getTestNotExist()),
-				HttpStatus.BAD_REQUEST);
-	}
+  // Get Test by Id API
+  @GetMapping(value = EndpointURI.GET_TEST_BY_ID)
+  public ResponseEntity<Object> getTestById(@PathVariable Long id) {
+    if (testService.isTestExist(id)) {
+      logger.debug("Id found");
+      return new ResponseEntity<>(
+          new ContentResponse<>(Constants.TEST,
+              mapper.map(testService.getTestById(id), TestDto.class), RestApiResponseStatus.OK),
+          HttpStatus.OK);
+    }
+    logger.debug("Invalid id");
+    return new ResponseEntity<>(new ValidationFailureResponse(Constants.TEST,
+        validationFailureStatusCodes.getTestNotExist()), HttpStatus.BAD_REQUEST);
+  }
 
-	// update test api
-	@PutMapping(value = EndpointURI.TEST)
-	public ResponseEntity<Object> updateTest(@Valid @RequestBody TestDto testDto) {
-		if (testService.isTestExist(testDto.getName())) {
-			if (testService.isUpdatedTestExist(testDto.getId(), testDto.getName())) {
-				return new ResponseEntity<>(new ValidationFailureResponse(Constants.TEST,
-						validationFailureStatusCodes.getTestAlreadyExist()), HttpStatus.BAD_REQUEST);
-			}
-			testService.saveTest(mapper.map(testDto, Test.class));
-			return new ResponseEntity<>(new BasicResponse<>(RestApiResponseStatus.OK, Constants.UPDATE_TEST_SUCCESS),
-					HttpStatus.OK);
-		}
-		return new ResponseEntity<>(
-				new ValidationFailureResponse(Constants.TEST, validationFailureStatusCodes.getTestNotExist()),
-				HttpStatus.BAD_REQUEST);
-	}
+  // Update Test API
+  @PutMapping(value = EndpointURI.TEST)
+  public ResponseEntity<Object> updateTest(@Valid @RequestBody TestDto testDto) {
+    if (testService.isTestExist(testDto.getId())) {
+      if (testService.isUpdatedTestExist(testDto.getId(), testDto.getName())) {
+        return new ResponseEntity<>(new ValidationFailureResponse(Constants.TEST,
+            validationFailureStatusCodes.getTestAlreadyExist()), HttpStatus.BAD_REQUEST);
+      }
+      testService.saveTest(mapper.map(testDto, Test.class));
+      return new ResponseEntity<>(
+          new BasicResponse<>(RestApiResponseStatus.OK, Constants.UPDATE_TEST_SUCCESS),
+          HttpStatus.OK);
+    }
+    return new ResponseEntity<>(new ValidationFailureResponse(Constants.TEST,
+        validationFailureStatusCodes.getTestNotExist()), HttpStatus.BAD_REQUEST);
+  }
 }
