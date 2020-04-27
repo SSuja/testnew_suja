71212c1179b6d19a0910b5be78a233d35fd6dd8f
ramz7 <ramminarayanan7@gmail.com>
2020-04-27 15:39:19 +0530

    mix-design-search-api

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/querydsl/controller/EmployeeSearchController.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/querydsl/controller/EmployeeSearchController.java
@@ -2,12 +2,17 @@ package com.tokyo.supermix.querydsl.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 import com.tokyo.supermix.data.entities.Employee;
+import com.tokyo.supermix.data.entities.QEmployee;
+import com.tokyo.supermix.data.repositories.EmployeeRepository;
 import com.tokyo.supermix.server.services.EmployeeService;
 
 
@@ -15,6 +20,8 @@ import com.tokyo.supermix.server.services.EmployeeService;
 public class EmployeeSearchController {
   @Autowired
   private EmployeeService employeeService;
+  @Autowired
+  private EmployeeRepository employeeRepository;
 
   @GetMapping("/searchEmployee")
   public Page<Employee> getPersonsSimplified(
@@ -22,5 +29,35 @@ public class EmployeeSearchController {
       @RequestParam(name = "page", defaultValue = "0") int page,
       @RequestParam(name = "size", defaultValue = "500") int size) {
     return employeeService.searchEmployee(predicate, page, size);
+  }
+
+  @GetMapping("/search")
+  public Page<Employee> getMixDesignInbetweenOperation(
+      @RequestParam(name = "page", defaultValue = "0") int page,
+      @RequestParam(name = "size", defaultValue = "500") int size,
+      @RequestParam(name = "firstName", required = false) String firstName,
+      @RequestParam(name = "lastName", required = false) String lastName,
+      @RequestParam(name = "email", required = false) String email,
+      @RequestParam(name = "phoneNumber", required = false) String phoneNumber,
+      @RequestParam(name = "address", required = false) String address) {
+    BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+    if (firstName != null && !firstName.isEmpty()) {
+      booleanBuilder.and(QEmployee.employee.firstName.eq(firstName));
+    }
+    if (lastName != null && !lastName.isEmpty()) {
+      booleanBuilder.and(QEmployee.employee.lastName.eq(lastName));
+    }
+    if (email != null && !email.isEmpty()) {
+      booleanBuilder.and(QEmployee.employee.email.eq(email));
+    }
+    if (phoneNumber != null && !phoneNumber.isEmpty()) {
+      booleanBuilder.and(QEmployee.employee.phoneNumber.eq(phoneNumber));
+    }
+    if (address != null && !address.isEmpty()) {
+      booleanBuilder.and(QEmployee.employee.address.eq(address));
+    }
+    return employeeRepository.findAll(booleanBuilder.getValue(),
+        PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "id")));
   }
 }
---
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/querydsl/controller/MixDesignSearchController.java
@@ -0,0 +1,38 @@
+package com.tokyo.supermix.querydsl.controller;
+
+import java.sql.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.querydsl.core.BooleanBuilder;
+import com.tokyo.supermix.data.entities.MixDesign;
+import com.tokyo.supermix.data.repositories.MixDesignRepository;
+import com.tokyo.supermix.server.services.MixDesignService;
+
+
+@RestController
+public class MixDesignSearchController {
+  @Autowired
+  private MixDesignRepository mixDesignRepository;
+
+  @Autowired
+  private MixDesignService mixDesignService;
+
+  @GetMapping("/search-mixdesign")
+  public Iterable<MixDesign> getMixDesignInbetweenOperation(
+      @RequestParam(name = "code", required = false) String code,
+      @RequestParam(name = "targetGrade", required = false) Double targetGrade,
+      @RequestParam(name = "date", required = false) Date date,
+      @RequestParam(name = "targetSlumpEqual", required = false) Double targetSlumpEqual,
+      @RequestParam(name = "targetSlumpMin", required = false) Double targetSlumpMin,
+      @RequestParam(name = "targetSlumpMax", required = false) Double targetSlumpMax,
+      @RequestParam(name = "waterCementRatio", required = false) Double waterCementRatio,
+      @RequestParam(name = "waterBinderRatio", required = false) Double waterBinderRatio,
+      @RequestParam(name = "plantCode", required = false) String plantCode) {
+    BooleanBuilder booleanBuilder = new BooleanBuilder();
+    mixDesignService.searchMixDesign(targetSlumpMin, targetSlumpMax, targetSlumpEqual, plantCode,
+        booleanBuilder);
+    return mixDesignRepository.findAll(booleanBuilder.getValue());
+  }
+}
