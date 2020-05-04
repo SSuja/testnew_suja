0d76cb121edf727b85a7bc16686a614a6438ea60
pakee san <sanpakees@gmail.com>
2020-05-04 17:24:41 +0530

    conflict resolved

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/MixDesignController.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/MixDesignController.java
@@ -117,8 +117,6 @@ public class MixDesignController {
       @RequestParam(name = "waterBinderRatioEqual", required = false) Double waterBinderRatioEqual,
       @RequestParam(name = "waterBinderRatioMin", required = false) Double waterBinderRatioMin,
       @RequestParam(name = "waterBinderRatioMax", required = false) Double waterBinderRatioMax,
-      @RequestParam(name = "waterCementRatio", required = false) Double waterCementRatio,
-      @RequestParam(name = "waterBinderRatio", required = false) Double waterBinderRatio,
       @RequestParam(name = "plantName", required = false) String plantName) {
     BooleanBuilder booleanBuilder = new BooleanBuilder();
     return mixDesignService.searchMixDesign(targetSlumpMin, targetSlumpMax, targetSlumpEqual,
