a2cb70d9bb3b5b904c57341937e78e48bfc5b2a1
ramz7 <ramminarayanan7@gmail.com>
2020-05-05 12:30:47 +0530

    mixDesign proportion search

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/MixDesignProportionService.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/MixDesignProportionService.java
@@ -1,14 +1,26 @@
 package com.tokyo.supermix.server.services;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
 import com.tokyo.supermix.data.entities.MixDesignProportion;
 
 public interface MixDesignProportionService {
   public List<MixDesignProportion> getAllMixDesignProportions();
+
   public MixDesignProportion saveMixDesignProportion(MixDesignProportion mixDesignProportion);
+
   public void deleteById(Long id);
+
   public MixDesignProportion getMixDesignProportionById(Long id);
+
   public boolean isMixDesignProportionExist(Long id);
+
   public List<MixDesignProportion> findByMixDesignCode(String mixDesignCode);
+
   public boolean existsByMixDesignCode(String mixDesignCode);
+
+  public Page<MixDesignProportion> searchMixDesignProportion(String rawMaterialName1,
+      String rawMaterialName2, String rawMaterialName3, String rawMaterialName4,
+      String rawMaterialName5, int page, int size, String mixDesignCode, Double targetGrade,
+      Double targetSlump);
 }
--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/MixDesignProportionServiceImpl.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/MixDesignProportionServiceImpl.java
@@ -2,10 +2,15 @@ package com.tokyo.supermix.server.services;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import com.querydsl.core.BooleanBuilder;
 import com.tokyo.supermix.data.entities.MixDesignProportion;
+import com.tokyo.supermix.data.entities.QMixDesignProportion;
 import com.tokyo.supermix.data.repositories.MixDesignProportionRepository;
 
 @Service
@@ -46,8 +51,57 @@ public class MixDesignProportionServiceImpl implements MixDesignProportionServic
 
   @Transactional(readOnly = true)
   public boolean existsByMixDesignCode(String mixDesignCode) {
-      return mixDesignProportionRepository.existsByMixDesignCode(mixDesignCode);
+    return mixDesignProportionRepository.existsByMixDesignCode(mixDesignCode);
   }
-  
 
+  @Transactional(readOnly = true)
+  public Page<MixDesignProportion> searchMixDesignProportion(String rawMaterialName1,
+      String rawMaterialName2, String rawMaterialName3, String rawMaterialName4,
+      String rawMaterialName5, int page, int size, String mixDesignCode, Double targetGrade,
+      Double targetSlump) {
+    BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+    if (rawMaterialName1 != null || rawMaterialName2 != null || rawMaterialName3 != null
+        || rawMaterialName4 != null) {
+      if (targetGrade != null) {
+        booleanBuilder
+            .and(QMixDesignProportion.mixDesignProportion.mixDesign.targetGrade.eq(targetGrade));
+      }
+      if (targetSlump != null) {
+        booleanBuilder
+            .and(QMixDesignProportion.mixDesignProportion.mixDesign.targetSlump.eq(targetSlump));
+      }
+      if (mixDesignCode != null && !mixDesignCode.isEmpty()) {
+        booleanBuilder
+            .and(QMixDesignProportion.mixDesignProportion.mixDesign.code.eq(mixDesignCode));
+      }
+    }
+    if (rawMaterialName1 != null && rawMaterialName2 == null && rawMaterialName3 == null
+        && rawMaterialName4 == null && rawMaterialName5 == null) {
+      booleanBuilder
+          .and(QMixDesignProportion.mixDesignProportion.rawMaterial.name.eq(rawMaterialName1));
+    }
+    if (rawMaterialName2 != null && rawMaterialName1 != null && rawMaterialName3 == null
+        && rawMaterialName4 == null && rawMaterialName5 == null) {
+      booleanBuilder
+          .and(QMixDesignProportion.mixDesignProportion.rawMaterial.name.eq(rawMaterialName2));
+    }
+    if (rawMaterialName3 != null && rawMaterialName1 != null && rawMaterialName2 != null
+        && rawMaterialName4 == null && rawMaterialName5 == null) {
+      booleanBuilder
+          .and(QMixDesignProportion.mixDesignProportion.rawMaterial.name.eq(rawMaterialName3));
+    }
+    if (rawMaterialName4 != null && rawMaterialName1 != null && rawMaterialName2 != null
+        && rawMaterialName3 != null && rawMaterialName5 == null) {
+      booleanBuilder
+          .and(QMixDesignProportion.mixDesignProportion.rawMaterial.name.eq(rawMaterialName4));
+    }
+    if (rawMaterialName5 != null && rawMaterialName4 != null && rawMaterialName1 != null
+        && rawMaterialName2 != null && rawMaterialName3 != null) {
+      booleanBuilder
+          .and(QMixDesignProportion.mixDesignProportion.rawMaterial.name.eq(rawMaterialName5));
+    }
+    return mixDesignProportionRepository.findAll(booleanBuilder.getValue(),
+        PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "id")));
+  }
 }
