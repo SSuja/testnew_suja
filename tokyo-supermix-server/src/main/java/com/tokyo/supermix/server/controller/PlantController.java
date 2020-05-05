91016b8040904455bb481cffb87407da245476f0
geerthana65 <geerthana65@gmail.com>
2020-05-05 09:54:09 +0530

    search api - plant

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/PlantController.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/PlantController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import javax.validation.Valid;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.querydsl.core.types.Predicate;
 import com.tokyo.supermix.EndpointURI;
 import com.tokyo.supermix.data.dto.PlantDto;
 import com.tokyo.supermix.data.entities.Plant;
@@ -29,85 +32,92 @@ import com.tokyo.supermix.util.ValidationFailureStatusCodes;
 @RestController
 @CrossOrigin
 public class PlantController {
-	@Autowired
-	PlantService plantService;
+  @Autowired
+  PlantService plantService;
 
-	@Autowired
-	ValidationFailureStatusCodes validationFailureStatusCodes;
+  @Autowired
+  ValidationFailureStatusCodes validationFailureStatusCodes;
 
-	@Autowired
-	private Mapper mapper;
+  @Autowired
+  private Mapper mapper;
 
-	private static final Logger logger = Logger.getLogger(PlantController.class);
+  private static final Logger logger = Logger.getLogger(PlantController.class);
 
-	@PostMapping(value = EndpointURI.PLANT)
-	public ResponseEntity<Object> createPlant(@Valid @RequestBody PlantDto plantDto) {
-		if (plantService.isPlantNameExist(plantDto.getName())) {
-			logger.debug("PlantName already exists: createPlant(), plantName: {}");
-			return new ResponseEntity<>(new ValidationFailureResponse(Constants.PLANT_NAME,
-					validationFailureStatusCodes.getPlantNameAlreadyExist()), HttpStatus.BAD_REQUEST);
-		}
-		if (plantService.isPlantExist(plantDto.getCode())) {
-			logger.debug("PlantId already exists: createPlant(), plantId: {}");
-			return new ResponseEntity<>(new ValidationFailureResponse(Constants.PLANT_ID,
-					validationFailureStatusCodes.getPlantIdAlreadyExist()), HttpStatus.BAD_REQUEST);
-		}
-		Plant plant = mapper.map(plantDto, Plant.class);
-		plantService.savePlant(plant);
-		return new ResponseEntity<>(new BasicResponse<>(RestApiResponseStatus.OK, Constants.ADD_PLANT_SUCCESS),
-				HttpStatus.OK);
-	}
+  @PostMapping(value = EndpointURI.PLANT)
+  public ResponseEntity<Object> createPlant(@Valid @RequestBody PlantDto plantDto) {
+    if (plantService.isPlantNameExist(plantDto.getName())) {
+      logger.debug("PlantName already exists: createPlant(), plantName: {}");
+      return new ResponseEntity<>(new ValidationFailureResponse(Constants.PLANT_NAME,
+          validationFailureStatusCodes.getPlantNameAlreadyExist()), HttpStatus.BAD_REQUEST);
+    }
+    if (plantService.isPlantExist(plantDto.getCode())) {
+      logger.debug("PlantId already exists: createPlant(), plantId: {}");
+      return new ResponseEntity<>(new ValidationFailureResponse(Constants.PLANT_ID,
+          validationFailureStatusCodes.getPlantIdAlreadyExist()), HttpStatus.BAD_REQUEST);
+    }
+    Plant plant = mapper.map(plantDto, Plant.class);
+    plantService.savePlant(plant);
+    return new ResponseEntity<>(
+        new BasicResponse<>(RestApiResponseStatus.OK, Constants.ADD_PLANT_SUCCESS), HttpStatus.OK);
+  }
 
-	@GetMapping(value = EndpointURI.PLANTS)
-	public ResponseEntity<Object> getAllPlants() {
-		List<Plant> plantList = plantService.getAllPlants();
-		List<PlantDto> plantDtoList = mapper.map(plantList, PlantDto.class);
-		return new ResponseEntity<>(new ContentResponse<>(Constants.PLANTS, plantDtoList, RestApiResponseStatus.OK),
-				null, HttpStatus.OK);
-	}
+  @GetMapping(value = EndpointURI.PLANTS)
+  public ResponseEntity<Object> getAllPlants() {
+    List<Plant> plantList = plantService.getAllPlants();
+    List<PlantDto> plantDtoList = mapper.map(plantList, PlantDto.class);
+    return new ResponseEntity<>(
+        new ContentResponse<>(Constants.PLANTS, plantDtoList, RestApiResponseStatus.OK), null,
+        HttpStatus.OK);
+  }
 
-	// get plant by id
-	@GetMapping(value = EndpointURI.GET_PLANT_BY_CODE)
-	public ResponseEntity<Object> getPlantByCode(@PathVariable String code) {
-		if (plantService.isPlantExist(code)) {
-			logger.debug("Get plant by plantCode ");
-			PlantDto plantDto = mapper.map(plantService.getPlantByCode(code), PlantDto.class);
-			return new ResponseEntity<>(new ContentResponse<>(Constants.PLANT, mapper.map(plantDto, PlantDto.class),
-					RestApiResponseStatus.OK), HttpStatus.OK);
-		}
-		return new ResponseEntity<>(
-				new ValidationFailureResponse(Constants.PLANT_ID, validationFailureStatusCodes.getPlantNotExist()),
-				HttpStatus.BAD_REQUEST);
-	}
+  // get plant by id
+  @GetMapping(value = EndpointURI.GET_PLANT_BY_CODE)
+  public ResponseEntity<Object> getPlantByCode(@PathVariable String code) {
+    if (plantService.isPlantExist(code)) {
+      logger.debug("Get plant by plantCode ");
+      PlantDto plantDto = mapper.map(plantService.getPlantByCode(code), PlantDto.class);
+      return new ResponseEntity<>(new ContentResponse<>(Constants.PLANT,
+          mapper.map(plantDto, PlantDto.class), RestApiResponseStatus.OK), HttpStatus.OK);
+    }
+    return new ResponseEntity<>(new ValidationFailureResponse(Constants.PLANT_ID,
+        validationFailureStatusCodes.getPlantNotExist()), HttpStatus.BAD_REQUEST);
+  }
 
-	@PutMapping(value = EndpointURI.PLANT)
-	public ResponseEntity<Object> updatePlant(@Valid @RequestBody PlantDto plantDto) {
-		if (plantService.isPlantExist(plantDto.getCode())) {
-			if (plantService.isUpdatedPlantNameExist(plantDto.getCode(), plantDto.getName())) {
-				return new ResponseEntity<>(new ValidationFailureResponse(Constants.PLANT_NAME,
-						validationFailureStatusCodes.getPlantAlreadyExist()), HttpStatus.BAD_REQUEST);
-			}
-			Plant plant = mapper.map(plantDto, Plant.class);
-			plantService.savePlant(plant);
-			return new ResponseEntity<>(new BasicResponse<>(RestApiResponseStatus.OK, Constants.UPDATE_PLANT_SUCCESS),
-					HttpStatus.OK);
-		}
-		return new ResponseEntity<>(
-				new ValidationFailureResponse(Constants.PLANT_NAME, validationFailureStatusCodes.getPlantNotExist()),
-				HttpStatus.BAD_REQUEST);
-	}
+  @PutMapping(value = EndpointURI.PLANT)
+  public ResponseEntity<Object> updatePlant(@Valid @RequestBody PlantDto plantDto) {
+    if (plantService.isPlantExist(plantDto.getCode())) {
+      if (plantService.isUpdatedPlantNameExist(plantDto.getCode(), plantDto.getName())) {
+        return new ResponseEntity<>(new ValidationFailureResponse(Constants.PLANT_NAME,
+            validationFailureStatusCodes.getPlantAlreadyExist()), HttpStatus.BAD_REQUEST);
+      }
+      Plant plant = mapper.map(plantDto, Plant.class);
+      plantService.savePlant(plant);
+      return new ResponseEntity<>(
+          new BasicResponse<>(RestApiResponseStatus.OK, Constants.UPDATE_PLANT_SUCCESS),
+          HttpStatus.OK);
+    }
+    return new ResponseEntity<>(new ValidationFailureResponse(Constants.PLANT_NAME,
+        validationFailureStatusCodes.getPlantNotExist()), HttpStatus.BAD_REQUEST);
+  }
 
-	@DeleteMapping(value = EndpointURI.DELETE_PLANT_BY_CODE)
-	public ResponseEntity<Object> deletePlant(@PathVariable String code) {
-		if (plantService.isPlantExist(code)) {
-			plantService.deletePlant(code);
-			return new ResponseEntity<>(new BasicResponse<>(RestApiResponseStatus.OK, Constants.PLANT_DELETED),
-					HttpStatus.OK);
-		}
-		logger.debug("Invalid Id");
-		return new ResponseEntity<>(
-				new ValidationFailureResponse(Constants.PLANT, validationFailureStatusCodes.getPlantNotExist()),
-				HttpStatus.BAD_REQUEST);
+  @DeleteMapping(value = EndpointURI.DELETE_PLANT_BY_CODE)
+  public ResponseEntity<Object> deletePlant(@PathVariable String code) {
+    if (plantService.isPlantExist(code)) {
+      plantService.deletePlant(code);
+      return new ResponseEntity<>(
+          new BasicResponse<>(RestApiResponseStatus.OK, Constants.PLANT_DELETED), HttpStatus.OK);
+    }
+    logger.debug("Invalid Id");
+    return new ResponseEntity<>(new ValidationFailureResponse(Constants.PLANT,
+        validationFailureStatusCodes.getPlantNotExist()), HttpStatus.BAD_REQUEST);
+  }
 
-	}
+  @GetMapping(value = EndpointURI.SEARCH_PLANT)
+  public ResponseEntity<Object> getPlantSearch(
+      @QuerydslPredicate(root = Plant.class) Predicate predicate,
+      @RequestParam(name = "page") int page, @RequestParam(name = "size") int size) {
+    return new ResponseEntity<>(new ContentResponse<>(Constants.PLANTS,
+        plantService.searchPlant(predicate, size, page), RestApiResponseStatus.OK), null,
+        HttpStatus.OK);
+  }
 }
