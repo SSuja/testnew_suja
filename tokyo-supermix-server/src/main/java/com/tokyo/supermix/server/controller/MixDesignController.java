ec206f512065f1c839284f91ef142fe6e61d48ca
ramz7 <ramminarayanan7@gmail.com>
2020-05-05 14:26:45 +0530

    delete unwanted import

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/MixDesignController.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/MixDesignController.java
@@ -4,7 +4,6 @@ import java.sql.Date;
 import javax.validation.Valid;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
