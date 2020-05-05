de1b718ef4fd232b971d0dfcc1ed6e6ab26512b3
pakee san <sanpakees@gmail.com>
2020-05-05 12:53:44 +0530

    PlantEquipment Search And RawMaterial Search

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/PlantEquipmentServiceImpl.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/PlantEquipmentServiceImpl.java
@@ -2,11 +2,16 @@ package com.tokyo.supermix.server.services;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import com.querydsl.core.types.Predicate;
 import com.tokyo.supermix.data.entities.PlantEquipment;
 import com.tokyo.supermix.data.repositories.PlantEquipmentRepository;
+
 
 @Service
 public class PlantEquipmentServiceImpl implements PlantEquipmentService {
@@ -41,6 +46,12 @@ public class PlantEquipmentServiceImpl implements PlantEquipmentService {
   public PlantEquipment getPlantEquipmentBySerialNo(String serialNo) {
 
     return PlantEquipmentRepository.findPlantEquipmentBySerialNo(serialNo);
+  }
+
+  @Transactional(readOnly = true)
+  public Page<PlantEquipment> searchPlantEquipment(Predicate predicate, int page, int size) {
+    return PlantEquipmentRepository.findAll(predicate,
+        PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "serialNo")));
   }
 
 }
--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/RawMaterialServiceImpl.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/RawMaterialServiceImpl.java
@@ -2,9 +2,13 @@ package com.tokyo.supermix.server.services;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import com.querydsl.core.types.Predicate;
 import com.tokyo.supermix.data.entities.RawMaterial;
 import com.tokyo.supermix.data.repositories.RawMaterialRepository;
 
@@ -49,5 +53,11 @@ public class RawMaterialServiceImpl implements RawMaterialService {
   @Transactional(propagation = Propagation.NEVER)
   public void deleteRawMaterial(Long id) {
     rawMaterialRepository.deleteById(id);
+  }
+
+  @Override
+  public Page<RawMaterial> searchRawMaterial(Predicate predicate, int page, int size) {
+    return rawMaterialRepository.findAll(predicate,
+        PageRequest.of(page, size, Sort.Direction.ASC, "id"));
   }
 }
