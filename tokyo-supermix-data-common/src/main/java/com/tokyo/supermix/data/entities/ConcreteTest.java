07e75a7f144c336179e441fad5088e6da8e3abf7
Mathuragini <mathuthiyasharma@gmail.com>
2020-05-06 16:21:12 +0530

    enitity-relationship-changed

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/ConcreteTest.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/ConcreteTest.java
@@ -1,6 +1,7 @@
 package com.tokyo.supermix.data.entities;
 
 import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -10,100 +11,101 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
 import com.tokyo.supermix.data.enums.Status;
 
 @Entity
 @Table(schema = "tokyo-supermix", name = "concrete_test")
 public class ConcreteTest implements Serializable {
-  private static final long serialVersionUID = 1L;
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
-  private Double slump;
-  private String temperature;
-  private Double waterContent;
-  private Double slumpGradeRatio;
-  private Double waterCementRatio;
-  private Double waterBinderRatio;
-  @Enumerated(EnumType.ORDINAL)
-  private Status status;
-  @OneToOne
-  @JoinColumn(name = "mixDesignCode", nullable = false)
-  private MixDesign mixDesign;
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private Double slump;
+	private String temperature;
+	private Double waterContent;
+	private Double slumpGradeRatio;
+	private Double waterCementRatio;
+	private Double waterBinderRatio;
+	@Enumerated(EnumType.ORDINAL)
+	private Status status;
+	@OneToOne
+	@JoinColumn(name = "finishProductSampleId", nullable = false)
+	private FinishProductSample finishProductSample;
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public Double getSlump() {
-    return slump;
-  }
+	public Double getSlump() {
+		return slump;
+	}
 
-  public void setSlump(Double slump) {
-    this.slump = slump;
-  }
+	public void setSlump(Double slump) {
+		this.slump = slump;
+	}
 
-  public String getTemperature() {
-    return temperature;
-  }
+	public String getTemperature() {
+		return temperature;
+	}
 
-  public void setTemperature(String temperature) {
-    this.temperature = temperature;
-  }
+	public void setTemperature(String temperature) {
+		this.temperature = temperature;
+	}
 
-  public Double getWaterContent() {
-    return waterContent;
-  }
+	public Double getWaterContent() {
+		return waterContent;
+	}
 
-  public void setWaterContent(Double waterContent) {
-    this.waterContent = waterContent;
-  }
+	public void setWaterContent(Double waterContent) {
+		this.waterContent = waterContent;
+	}
 
-  public Double getSlumpGradeRatio() {
-    return slumpGradeRatio;
-  }
+	public Double getSlumpGradeRatio() {
+		return slumpGradeRatio;
+	}
 
-  public void setSlumpGradeRatio(Double slumpGradeRatio) {
-    this.slumpGradeRatio = slumpGradeRatio;
-  }
+	public void setSlumpGradeRatio(Double slumpGradeRatio) {
+		this.slumpGradeRatio = slumpGradeRatio;
+	}
 
-  public Double getWaterCementRatio() {
-    return waterCementRatio;
-  }
+	public Double getWaterCementRatio() {
+		return waterCementRatio;
+	}
 
-  public void setWaterCementRatio(Double waterCementRatio) {
-    this.waterCementRatio = waterCementRatio;
-  }
+	public void setWaterCementRatio(Double waterCementRatio) {
+		this.waterCementRatio = waterCementRatio;
+	}
 
-  public Double getWaterBinderRatio() {
-    return waterBinderRatio;
-  }
+	public Double getWaterBinderRatio() {
+		return waterBinderRatio;
+	}
 
-  public void setWaterBinderRatio(Double waterBinderRatio) {
-    this.waterBinderRatio = waterBinderRatio;
-  }
+	public void setWaterBinderRatio(Double waterBinderRatio) {
+		this.waterBinderRatio = waterBinderRatio;
+	}
 
-  public Status getStatus() {
-    return status;
-  }
+	public Status getStatus() {
+		return status;
+	}
 
-  public void setStatus(Status status) {
-    this.status = status;
-  }
+	public void setStatus(Status status) {
+		this.status = status;
+	}
 
-  public MixDesign getMixDesign() {
-    return mixDesign;
-  }
+	public FinishProductSample getFinishProductSample() {
+		return finishProductSample;
+	}
 
-  public void setMixDesign(MixDesign mixDesign) {
-    this.mixDesign = mixDesign;
-  }
+	public void setFinishProductSample(FinishProductSample finishProductSample) {
+		this.finishProductSample = finishProductSample;
+	}
 
-  public static long getSerialversionuid() {
-    return serialVersionUID;
-  }
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 }
