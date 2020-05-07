62008e44062bad3b6db1dc8ffb76914b812c1182
thevakajan <thevakajan3.com>
2020-05-07 11:37:40 +0530

    Role api

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/RoleRepository.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/RoleRepository.java
@@ -2,11 +2,10 @@ package com.tokyo.supermix.data.repositories;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.tokyo.supermix.data.entities.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 	Optional<Role> findByRoleName(String roleName);
-
 	Boolean existsByRoleName(String roleName);
+	Boolean existsByRoleNameAndId(String roleName, Long Id);
 }
--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/RoleServiceImpl.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/RoleServiceImpl.java
@@ -1,48 +1,58 @@
 package com.tokyo.supermix.server.services;
 
 import java.util.List;
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.tokyo.supermix.data.entities.Role;
 import com.tokyo.supermix.data.repositories.RoleRepository;
 
 @Service
 public class RoleServiceImpl implements RoleService {
-	
-	@Autowired
-	RoleRepository roleRepository;
 
-	@Transactional
-	  public void saveRole(Role role) {
-	   roleRepository.save(role);
-	  }
-	
-	@Transactional(readOnly = true)
-	public List<Role> viewAllRole() {
-		return roleRepository.findAll();
-	}
+  @Autowired
+  private RoleRepository roleRepository;
 
-	@Transactional(propagation = Propagation.NEVER)
-	public Long deleteRole(Long id) {
-		if (roleRepository.getOne(id) != null) {
-			roleRepository.deleteById(id);
-			return id;
-		}
-		return null;
-	}
-	@Transactional(readOnly = true)
-	public Role findRoleById(Long id) {
-		return roleRepository.findById(id).get();
-	}
-	@Transactional(readOnly = true)
-	public Boolean existsByRole(String roleName) {
-		return roleRepository.existsByRoleName(roleName);
-	}
+  @Transactional
+  public void saveRole(Role role) {
+    roleRepository.save(role);
+  }
+
+  @Transactional(readOnly = true)
+  public List<Role> getAllRoles() {
+    return roleRepository.findAll();
+  }
+
+  @Transactional(propagation = Propagation.NEVER)
+  public void deleteRole(Long id) {
+    roleRepository.deleteById(id);
+  }
+
+
+  @Transactional(readOnly = true)
+  public Role findRoleById(Long id) {
+    return roleRepository.findById(id).get();
+  }
+
+  @Transactional(readOnly = true)
+  public boolean existsByRoleName(String roleName) {
+    return roleRepository.existsByRoleName(roleName);
+  }
+
+  @Transactional(readOnly = true)
+  public boolean isRoleExists(Long id) {
+    return roleRepository.existsById(id);
+  }
+
+  @Transactional(readOnly = true)
+  public boolean isUpdatedRoleExists(Long id, String roleName) {
+    if ((!findRoleById(id).getRoleName().equalsIgnoreCase(roleName))
+        && (existsByRoleName(roleName))) {
+      return true;
+    }
+    return false;
+  }
+
 
 }
