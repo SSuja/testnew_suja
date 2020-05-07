a1cd9e15ee4474e22b55ba40f70a5a3b05f42a25
geerthana65 <geerthana65@gmail.com>
2020-05-07 12:10:13 +0530

    removed custom query for incomingsample repository

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/IncomingSampleRepository.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/IncomingSampleRepository.java
@@ -4,36 +4,31 @@ import java.sql.Date;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import com.tokyo.supermix.data.entities.IncomingSample;
 import com.tokyo.supermix.data.enums.Status;
 
 public interface IncomingSampleRepository
-		extends JpaRepository<IncomingSample, String>, QuerydslPredicateExecutor<IncomingSample> {
-	boolean existsByCode(String code);
+    extends JpaRepository<IncomingSample, String>, QuerydslPredicateExecutor<IncomingSample> {
+  boolean existsByCode(String code);
 
-	IncomingSample findIncomingSampleByCode(String code);
+  IncomingSample findIncomingSampleByCode(String code);
 
-	List<IncomingSample> findIncomingSampleByStatus(Status status);
+  List<IncomingSample> findIncomingSampleByStatus(Status status);
 
-	boolean existsByStatus(Status status);
+  boolean existsByStatus(Status status);
 
-	List<IncomingSample> findByStatusAndRawMaterialIdAndDate(Status status, Long RawMaterialId, Date date);
+  List<IncomingSample> findByStatusAndRawMaterialIdAndDate(Status status, Long RawMaterialId,
+      Date date);
 
-	List<IncomingSample> findByRawMaterialIdAndDate(Long RawMaterialId, Date date);
+  List<IncomingSample> findByRawMaterialIdAndDate(Long RawMaterialId, Date date);
 
-	List<IncomingSample> findByStatus(Status status);
+  List<IncomingSample> findByStatus(Status status);
 
-	List<IncomingSample> findByRawMaterialMaterialSubCategoryIdAndDate(Long materialSubCategoryId, Date date);
+  List<IncomingSample> findByRawMaterialMaterialSubCategoryIdAndDate(Long materialSubCategoryId,
+      Date date);
 
-	List<IncomingSample> findByRawMaterialMaterialSubCategoryIdAndDateAndStatus(Long materialSubCategoryId, Date date,
-			Status status);
-
-	@Query(value = "SELECT COUNT(code) FROM incoming_sample inner join raw_material on incoming_sample.raw_material_id = raw_material.id inner join material_sub_category on raw_material.material_sub_category_id = material_sub_category.id inner join material_category on material_sub_category.material_category_id = material_category.id where incoming_sample.date = current_date() and material_category.name=?1", nativeQuery = true)
-	Long countByTotalMaterialCategoryIncomingSample(String materialCategoryName);
-
-	@Query(value = "SELECT COUNT(code) FROM incoming_sample inner join  raw_material on incoming_sample.raw_material_id = raw_material.id inner join material_sub_category on raw_material.material_sub_category_id = material_sub_category.id inner join material_category on material_sub_category.material_category_id = material_category.id where incoming_sample.date =current_date() and material_category.name=?1 and incoming_sample.status= ?2", nativeQuery = true)
-	Long getMaterialCategoryStatusCount(String materialCategoryName, int status);
+  List<IncomingSample> findByRawMaterialMaterialSubCategoryIdAndDateAndStatus(
+      Long materialSubCategoryId, Date date, Status status);
 }
