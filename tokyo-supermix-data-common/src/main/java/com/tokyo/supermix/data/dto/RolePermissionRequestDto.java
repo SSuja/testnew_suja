8375e86b61b6bb3264374cbc702bbef0b7d4cbcb
geerthana65 <geerthana65@gmail.com>
2020-05-15 11:30:48 +0530

    create role-permission entity & repository classes

---
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/dto/RolePermissionRequestDto.java
@@ -0,0 +1,32 @@
+package com.tokyo.supermix.data.dto;
+
+public class RolePermissionRequestDto {
+  private Long id;
+  private Long roleId;
+  private Long permissionId;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getRoleId() {
+    return roleId;
+  }
+
+  public void setRoleId(Long roleId) {
+    this.roleId = roleId;
+  }
+
+  public Long getPermissionId() {
+    return permissionId;
+  }
+
+  public void setPermissionId(Long permissionId) {
+    this.permissionId = permissionId;
+  }
+
+}
---
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/RolePermission.java
@@ -0,0 +1,53 @@
+package com.tokyo.supermix.data.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "tokyo-supermix", name = "role_permission")
+public class RolePermission implements Serializable {
+  private static final long serialVersionUID = 1L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  @ManyToOne
+  @JoinColumn(name = "role_id", nullable = false)
+  private Role role;
+  @ManyToOne
+  @JoinColumn(name = "permission_id", nullable = false)
+  private Permission permission;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Role getRole() {
+    return role;
+  }
+
+  public void setRole(Role role) {
+    this.role = role;
+  }
+
+  public Permission getPermission() {
+    return permission;
+  }
+
+  public void setPermission(Permission permission) {
+    this.permission = permission;
+  }
+
+  public static long getSerialversionuid() {
+    return serialVersionUID;
+  }
+}
