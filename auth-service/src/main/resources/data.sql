a8548cda918a586459767a93e4a91b370217b9d0
pakee san <sanpakees@gmail.com>
2020-05-29 23:49:46 +0530

    data-sql

--- auth-service/src/main/resources/data.sql
+++ auth-service/src/main/resources/data.sql
@@ -1,7 +1,6 @@
 --create Role 
 INSERT INTO `tokyo-supermix`.`role` (`id`, `role_name`) VALUES ('1', 'ADMIN');
-INSERT INTO `tokyo-supermix`.`role` (`id`, `role_name`) VALUES ('2', 'EMPLOYEE');
-INSERT INTO `tokyo-supermix`.`role` (`id`, `role_name`) VALUES ('3', 'HR');
+
 --create User 
 INSERT INTO `tokyo-supermix`.`user` (`id`, `email`, `password`, `user_name`, `role_id`, `created_at`, `updated_at`) VALUES ('1', 'admin@gmail.com', '$2y$10$WYI8/0dTM5y.0VZKCEbFWuU1Y39zOx6V3oFj6EZvh6AGXp0T.VQVK', 'admin', '1', '2020-05-06 16:58:02', '2020-05-06 21:28:58');
 --admin pwd --
@@ -207,7 +206,6 @@ INSERT INTO `tokyo-supermix`.`permission` (`id`, `name`) VALUES ('164', 'get_pla
 INSERT INTO `tokyo-supermix`.`permission` (`id`, `name`) VALUES ('165', 'add_plant_equipment_calibration');
 INSERT INTO `tokyo-supermix`.`permission` (`id`, `name`) VALUES ('166', 'edit_plant_equipment_calibration');
 INSERT INTO `tokyo-supermix`.`permission` (`id`, `name`) VALUES ('167','delete_plant_equipment_calibration');
-
 
 
 
