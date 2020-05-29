64bf9504c085624f9a9689dd479d64d7ebae129d
ramz7 <ramminarayanan7@gmail.com>
2020-05-29 16:28:39 +0530

    annotation added

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/TokyoSupermixServer.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/TokyoSupermixServer.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableEurekaClient
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class TokyoSupermixServer {
 
     public static void main(String[] args) {
