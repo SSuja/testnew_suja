c575ecc1c23f0f19c93fbedd033362383873c123
geerthana65 <geerthana65@gmail.com>
2020-06-05 17:09:27 +0530

    get incoming sample by material category

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/IncomingSampleServiceImpl.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/IncomingSampleServiceImpl.java
@@ -19,87 +19,95 @@ import com.tokyo.supermix.data.repositories.IncomingSampleRepository;
 
 @Service
 public class IncomingSampleServiceImpl implements IncomingSampleService {
-	@Autowired
-	private IncomingSampleRepository incomingSampleRepository;
+  @Autowired
+  private IncomingSampleRepository incomingSampleRepository;
 
-	@Transactional
-	public void createIncomingSample(IncomingSample incomingSample) {
-		if (incomingSample.getCode() == null) {
-			String codePrefix = incomingSample.getPlant().getCode() + "-INC-";
-			List<IncomingSample> incomingSampleList = incomingSampleRepository.findByCodeContaining(codePrefix);
-			if (incomingSampleList.size() == 0) {
-				incomingSample.setCode(codePrefix + String.format("%04d", 1));
-			} else {
-				incomingSample.setCode(codePrefix + String.format("%04d", maxNumberFromCode(incomingSampleList) + 1));
-			}
-		}
-		incomingSample.setStatus(Status.NEW);
-		incomingSampleRepository.save(incomingSample);
-	}
+  @Transactional
+  public void createIncomingSample(IncomingSample incomingSample) {
+    if (incomingSample.getCode() == null) {
+      String codePrefix = incomingSample.getPlant().getCode() + "-INC-";
+      List<IncomingSample> incomingSampleList =
+          incomingSampleRepository.findByCodeContaining(codePrefix);
+      if (incomingSampleList.size() == 0) {
+        incomingSample.setCode(codePrefix + String.format("%04d", 1));
+      } else {
+        incomingSample
+            .setCode(codePrefix + String.format("%04d", maxNumberFromCode(incomingSampleList) + 1));
+      }
+    }
+    incomingSample.setStatus(Status.NEW);
+    incomingSampleRepository.save(incomingSample);
+  }
 
-	private Integer getNumberFromCode(String code) {
-		String numberOnly = code.replaceAll("[^0-9]", "");
-		return Integer.parseInt(numberOnly);
-	}
+  private Integer getNumberFromCode(String code) {
+    String numberOnly = code.replaceAll("[^0-9]", "");
+    return Integer.parseInt(numberOnly);
+  }
 
-	private Integer maxNumberFromCode(List<IncomingSample> incomingSampleList) {
-		List<Integer> list = new ArrayList<Integer>();
-		incomingSampleList.forEach(obj -> {
-			list.add(getNumberFromCode(obj.getCode()));
-		});
-		return Collections.max(list);
-	}
+  private Integer maxNumberFromCode(List<IncomingSample> incomingSampleList) {
+    List<Integer> list = new ArrayList<Integer>();
+    incomingSampleList.forEach(obj -> {
+      list.add(getNumberFromCode(obj.getCode()));
+    });
+    return Collections.max(list);
+  }
 
-	@Transactional
-	public void updateIncomingSample(IncomingSample incomingSample) {
-		incomingSampleRepository.save(incomingSample);
-	}
+  @Transactional
+  public void updateIncomingSample(IncomingSample incomingSample) {
+    incomingSampleRepository.save(incomingSample);
+  }
 
-	@Transactional(propagation = Propagation.NEVER)
-	public void deleteIncomingSample(String code) {
-		incomingSampleRepository.deleteById(code);
-	}
+  @Transactional(propagation = Propagation.NEVER)
+  public void deleteIncomingSample(String code) {
+    incomingSampleRepository.deleteById(code);
+  }
 
-	@Transactional(readOnly = true)
-	public IncomingSample getIncomingSampleById(String code) {
-		return incomingSampleRepository.findById(code).get();
-	}
+  @Transactional(readOnly = true)
+  public IncomingSample getIncomingSampleById(String code) {
+    return incomingSampleRepository.findById(code).get();
+  }
 
-	@Transactional(readOnly = true)
-	public List<IncomingSample> getAllIncomingSamples() {
-		return incomingSampleRepository.findAll();
-	}
+  @Transactional(readOnly = true)
+  public List<IncomingSample> getAllIncomingSamples() {
+    return incomingSampleRepository.findAll();
+  }
 
-	@Transactional(readOnly = true)
-	public boolean isIncomingSampleExist(String code) {
-		return incomingSampleRepository.existsByCode(code);
-	}
+  @Transactional(readOnly = true)
+  public boolean isIncomingSampleExist(String code) {
+    return incomingSampleRepository.existsByCode(code);
+  }
 
-	@Transactional
-	public void updateStatusForIncomingSample(String code, Status status) {
-		IncomingSample incomingSample = incomingSampleRepository.findById(code).get();
-		incomingSample.setStatus(status);
-		incomingSampleRepository.save(incomingSample);
-	}
+  @Transactional
+  public void updateStatusForIncomingSample(String code, Status status) {
+    IncomingSample incomingSample = incomingSampleRepository.findById(code).get();
+    incomingSample.setStatus(status);
+    incomingSampleRepository.save(incomingSample);
+  }
 
-	@Transactional(readOnly = true)
-	public List<IncomingSample> getIncomingSampleByStatus(Status status) {
-		return incomingSampleRepository.findIncomingSampleByStatus(status);
-	}
+  @Transactional(readOnly = true)
+  public List<IncomingSample> getIncomingSampleByStatus(Status status) {
+    return incomingSampleRepository.findIncomingSampleByStatus(status);
+  }
 
-	@Transactional(readOnly = true)
-	public boolean isIncomingSampleStatusExist(Status status) {
-		return incomingSampleRepository.existsByStatus(status);
-	}
+  @Transactional(readOnly = true)
+  public boolean isIncomingSampleStatusExist(Status status) {
+    return incomingSampleRepository.existsByStatus(status);
+  }
 
-	@Transactional(readOnly = true)
-	public Page<IncomingSample> searchIncomingSample(Predicate predicate, int page, int size) {
-		return incomingSampleRepository.findAll(predicate,
-				PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "code")));
-	}
+  @Transactional(readOnly = true)
+  public Page<IncomingSample> searchIncomingSample(Predicate predicate, int page, int size) {
+    return incomingSampleRepository.findAll(predicate,
+        PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "code")));
+  }
 
-	@Transactional(readOnly = true)
-	public List<IncomingSample> getIncomingSampleByPlantCode(String plantCode) {
-		return incomingSampleRepository.findByPlantCode(plantCode);
-	}
+  @Transactional(readOnly = true)
+  public List<IncomingSample> getIncomingSampleByPlantCode(String plantCode) {
+    return incomingSampleRepository.findByPlantCode(plantCode);
+  }
+
+  @Transactional(readOnly = true)
+  public List<IncomingSample> getIncomingSampleByMaterialCategory(String materialCategoryName) {
+    return incomingSampleRepository
+        .findByRawMaterialMaterialSubCategoryMaterialCategoryName(materialCategoryName);
+  }
 }
