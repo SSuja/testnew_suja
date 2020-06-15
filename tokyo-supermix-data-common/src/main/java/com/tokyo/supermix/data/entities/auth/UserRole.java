d6552af9e92b1434e3f780a28622d2c1e10ae072
jananthan93 <jjananthan93@gmail.com>
2020-06-15 15:06:52 +0530

    user type enum and user role creation

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/auth/UserRole.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/auth/UserRole.java
@@ -1,37 +1,27 @@
-package com.tokyo.supermix.data.entities.privilege;
+package com.tokyo.supermix.data.entities.auth;
 
 import java.io.Serializable;
-import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import com.tokyo.supermix.data.entities.Plant;
-import com.tokyo.supermix.data.entities.auth.Role;
-import com.tokyo.supermix.data.entities.auth.UserPlantRole;
 
 @Entity
-@Table(name = "plant_role", schema = "tokyo-supermix")
-public class PlantRole implements Serializable {
+@Table(name = "user_role", schema = "tokyo-supermix")
+public class UserRole implements Serializable {
   private static final long serialVersionUID = 1L;
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Id
-  Long id;
-
+  private Long id;
   @ManyToOne
-  @JoinColumn(name = "plant_code")
-  private Plant plant;
-
+  @JoinColumn(name = "user_id")
+  private User user;
   @ManyToOne
   @JoinColumn(name = "role_id")
   private Role role;
-
-  @OneToMany(mappedBy = "plantRole")
-  private Set<UserPlantRole> userPlantRoles;
 
   public Long getId() {
     return id;
@@ -41,12 +31,12 @@ public class PlantRole implements Serializable {
     this.id = id;
   }
 
-  public Plant getPlant() {
-    return plant;
+  public User getUser() {
+    return user;
   }
 
-  public void setPlant(Plant plant) {
-    this.plant = plant;
+  public void setUser(User user) {
+    this.user = user;
   }
 
   public Role getRole() {
@@ -56,5 +46,4 @@ public class PlantRole implements Serializable {
   public void setRole(Role role) {
     this.role = role;
   }
-
 }
