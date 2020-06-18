c9173c96a0598696ac0f1e906032ff2d08107145
thevakajan <thevakajan3.com>
2020-06-18 18:34:27 +0530

    api

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/auth/UserPlantRoleRepository.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/auth/UserPlantRoleRepository.java
@@ -1,8 +1,16 @@
 package com.tokyo.supermix.data.repositories.auth;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.tokyo.supermix.data.entities.auth.UserPlantRole;
 
-public interface UserPlantRoleRepository extends JpaRepository<UserPlantRole, Long>{
+public interface UserPlantRoleRepository extends JpaRepository<UserPlantRole, Long> {
+  List<UserPlantRole> findRolesByUserId(Long userId);
+
+  List<UserPlantRole> findUsersByPlantRoleId(Long PlantRoleId);
+
+  boolean existsByPlantRoleId(Long plantRoleId);
+
+  boolean existsByUserId(Long userId);
 
 }
--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/auth/UserPlantRoleController.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/auth/UserPlantRoleController.java
@@ -1,10 +1,50 @@
 package com.tokyo.supermix.server.controller.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import com.tokyo.supermix.PrivilegeEndpointURI;
+import com.tokyo.supermix.rest.enums.RestApiResponseStatus;
+import com.tokyo.supermix.rest.response.ContentResponse;
+import com.tokyo.supermix.rest.response.ValidationFailureResponse;
+import com.tokyo.supermix.server.services.auth.UserPlantRoleService;
+import com.tokyo.supermix.util.privilege.PrivilegeConstants;
+import com.tokyo.supermix.util.privilege.PrivilegeValidationFailureStatusCodes;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class UserPlantRoleController {
+  
+  @Autowired
+  private UserPlantRoleService userPlantRoleService;
+  @Autowired
+  private PrivilegeValidationFailureStatusCodes privilegeValidationFailureStatusCodes;
+
+
+    @GetMapping(value = PrivilegeEndpointURI.USER_PLANT_ROLE_BY_USER)
+    public ResponseEntity<Object> getRolesByUserId(@PathVariable Long userId ){
+      if(userPlantRoleService.existsByUserId(userId)) {
+        return new ResponseEntity<>(new ContentResponse<>(PrivilegeConstants.ROLE,
+            userPlantRoleService.getRolesByUserId(userId), RestApiResponseStatus.OK),
+            HttpStatus.OK);
+      }
+      return new ResponseEntity<>(new ValidationFailureResponse(PrivilegeConstants.USER,
+          privilegeValidationFailureStatusCodes.getUserNotExist()), HttpStatus.BAD_REQUEST);
+    }
+    
+    @GetMapping(value = PrivilegeEndpointURI.USER_PLANT_ROLE_BY_ROLE)
+    public ResponseEntity<Object> UsersByPlantRoleId(@PathVariable Long plantRoleId){
+      if(userPlantRoleService.existsByPlantRoleId(plantRoleId)) {
+        return new ResponseEntity<>(new ContentResponse<>(PrivilegeConstants.ROLE,
+            userPlantRoleService.getUsersByPlantRoleId(plantRoleId), RestApiResponseStatus.OK),
+            HttpStatus.OK);
+      }
+      return new ResponseEntity<>(new ValidationFailureResponse(PrivilegeConstants.USER,
+          privilegeValidationFailureStatusCodes.getUserNotExist()), HttpStatus.BAD_REQUEST);
+    }
 
 }
