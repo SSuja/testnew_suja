66ae9e48c357d0b10f4173b1a6da78ef6562af0c
ramz7 <ramminarayanan7@gmail.com>
2020-06-23 10:22:09 +0530

    get all plant permission response changed

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/privilege/PlantPermissionServiceImpl.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/privilege/PlantPermissionServiceImpl.java
@@ -6,11 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.tokyo.supermix.data.dto.privilege.PlantPermissionResponseDto;
+import com.tokyo.supermix.data.dto.privilege.PlantRolePlantPermissionRequestDto;
+import com.tokyo.supermix.data.dto.privilege.PlantRolePlantPermissionResponseDto;
+import com.tokyo.supermix.data.dto.privilege.SubModulePlantRolePlantPermissionDto;
 import com.tokyo.supermix.data.entities.Plant;
+import com.tokyo.supermix.data.entities.privilege.MainModule;
 import com.tokyo.supermix.data.entities.privilege.Permission;
 import com.tokyo.supermix.data.entities.privilege.PlantPermission;
+import com.tokyo.supermix.data.entities.privilege.SubModule;
 import com.tokyo.supermix.data.repositories.auth.PermissionRepository;
+import com.tokyo.supermix.data.repositories.privilege.MainModuleRepository;
 import com.tokyo.supermix.data.repositories.privilege.PlantPermissionRepository;
+import com.tokyo.supermix.data.repositories.privilege.SubModuleRepository;
 
 @Service
 public class PlantPermissionServiceImpl implements PlantPermissionService {
@@ -18,6 +25,10 @@ public class PlantPermissionServiceImpl implements PlantPermissionService {
   private PlantPermissionRepository plantPermissionRepository;
   @Autowired
   private PermissionRepository permissionRepository;
+  @Autowired
+  private MainModuleRepository mainModuleRepository;
+  @Autowired
+  private SubModuleRepository subModuleRepository;
 
   @Transactional(readOnly = true)
   public List<String> getPlantsByPermissionName(String permissionName) {
@@ -31,8 +42,60 @@ public class PlantPermissionServiceImpl implements PlantPermissionService {
   }
 
   @Transactional(readOnly = true)
-  public List<PlantPermission> getAllPlantsByPermissions() {
-    return plantPermissionRepository.findAll();
+  public List<PlantRolePlantPermissionResponseDto> getAllPlantsByPermissions() {
+    return getPlantPermissionsByMainModule(mainModuleRepository.findAll());
+  }
+
+  private List<PlantRolePlantPermissionResponseDto> getPlantPermissionsByMainModule(
+      List<MainModule> findAll) {
+    List<PlantRolePlantPermissionResponseDto> plantPermissionMainModuleDtolist =
+        new ArrayList<PlantRolePlantPermissionResponseDto>();
+    mainModuleRepository.findAll().forEach(main -> {
+      PlantRolePlantPermissionResponseDto plantPermissionMainModuleDto =
+          new PlantRolePlantPermissionResponseDto();
+      plantPermissionMainModuleDto.setMainModule(main.getName());
+      plantPermissionMainModuleDto.setMainModuleId(main.getId());
+      List<SubModule> subModuleList = subModuleRepository.findByMainModuleId(main.getId());
+      plantPermissionMainModuleDto
+          .setSubModules(getPlantPermissionSubModulesByMainModuleId(subModuleList, main.getId()));;
+      plantPermissionMainModuleDtolist.add(plantPermissionMainModuleDto);
+    });
+    return plantPermissionMainModuleDtolist;
+  }
+
+  private List<SubModulePlantRolePlantPermissionDto> getPlantPermissionSubModulesByMainModuleId(
+      List<SubModule> subModuleList, Long mainModuleId) {
+    List<SubModulePlantRolePlantPermissionDto> SubModuleRolePermissionDtoList =
+        new ArrayList<SubModulePlantRolePlantPermissionDto>();
+    for (SubModule sub : subModuleList) {
+      SubModulePlantRolePlantPermissionDto plantPermissionSubModuleDto =
+          new SubModulePlantRolePlantPermissionDto();
+      plantPermissionSubModuleDto.setSubModule(sub.getName());
+      plantPermissionSubModuleDto.setSubModuleId(sub.getId());
+      plantPermissionSubModuleDto.setMainModuleId(mainModuleId);
+      plantPermissionSubModuleDto.setPlantRolePlantPermissions(
+          getPlantPermissionsBySubModuleId(sub.getId(), mainModuleId));
+      SubModuleRolePermissionDtoList.add(plantPermissionSubModuleDto);
+    }
+    return SubModuleRolePermissionDtoList;
+  }
+
+  private List<PlantRolePlantPermissionRequestDto> getPlantPermissionsBySubModuleId(
+      Long subModuleId, Long mainModuleId) {
+    List<PlantRolePlantPermissionRequestDto> plantPermissionResponseDtos =
+        new ArrayList<PlantRolePlantPermissionRequestDto>();
+    List<PlantPermission> plantPermissions =
+        plantPermissionRepository.findByPermissionSubModuleId(subModuleId);
+    for (PlantPermission permission : plantPermissions) {
+      PlantRolePlantPermissionRequestDto plantPermissionResponseDto =
+          new PlantRolePlantPermissionRequestDto();
+      plantPermissionResponseDto.setPlantPermissionName(permission.getPermission().getName());
+      plantPermissionResponseDto.setPlantPermissionId(permission.getId());
+      plantPermissionResponseDto.setMainModuleId(mainModuleId);
+      plantPermissionResponseDto.setSubModuleId(subModuleId);
+      plantPermissionResponseDtos.add(plantPermissionResponseDto);
+    }
+    return plantPermissionResponseDtos;
   }
 
   @Transactional
@@ -57,7 +120,7 @@ public class PlantPermissionServiceImpl implements PlantPermissionService {
         new ArrayList<PlantPermissionResponseDto>();
     plantPermissionList.forEach(plantPermission -> {
       PlantPermissionResponseDto plantPermissionResponseDto = new PlantPermissionResponseDto();
-      plantPermissionResponseDto.setId(plantPermission.getId());;
+      plantPermissionResponseDto.setId(plantPermission.getId());
       plantPermissionResponseDto.setName(plantPermission.getName());
       plantPermissionResponseDto.setPermissionName(plantPermission.getPermission().getName());
       plantPermissionResponseDto.setPlantCode(plantPermission.getPlant().getCode());
@@ -65,7 +128,8 @@ public class PlantPermissionServiceImpl implements PlantPermissionService {
     });
 
     return plantPermissionResponseDtolist;
-  } 
+  }
+
   public boolean isPermissionNameExists(String permissionName) {
     return plantPermissionRepository.existsByPermissionName(permissionName);
   }
