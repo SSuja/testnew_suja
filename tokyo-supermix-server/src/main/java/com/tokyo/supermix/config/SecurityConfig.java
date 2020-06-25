4776a2aa148884857c04bd937b0e0e7151e73b00
unknown <sujanthan006@gmail.com>
2020-06-25 11:07:56 +0530

    equipment

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/config/SecurityConfig.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/config/SecurityConfig.java
@@ -66,7 +66,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers(HttpMethod.POST,EndpointURI.MATERIAL_STATE).hasAnyAuthority(PermissionConstants.CREATE_MATERIAL_STATE)
         .antMatchers(HttpMethod.PUT,EndpointURI.MATERIAL_STATE).hasAnyAuthority(PermissionConstants.EDIT_MATERIAL_STATE)
         .antMatchers(HttpMethod.DELETE,EndpointURI.DELETE_MATERIAL_STATE).hasAnyAuthority(PermissionConstants.DELETE_MATERIAL_STATE)
-        ;
+        .antMatchers(HttpMethod.GET,EndpointURI.RAW_MATERIALS).hasAnyAuthority(PermissionConstants.VIEW_RAW_MATERIAL)
+        .antMatchers(HttpMethod.POST,EndpointURI.RAW_MATERIAL).hasAnyAuthority(PermissionConstants.CREATE_RAW_MATERIAL)
+        .antMatchers(HttpMethod.PUT,EndpointURI.RAW_MATERIAL).hasAnyAuthority(PermissionConstants.EDIT_RAW_MATERIAL)
+        .antMatchers(HttpMethod.DELETE,EndpointURI.DELETE_RAW_MATERIAL).hasAnyAuthority(PermissionConstants.DELETE_RAW_MATERIAL)
+        .antMatchers(HttpMethod.GET,EndpointURI.EQUIPMENTS).hasAnyAuthority(PermissionConstants.VIEW_EQUIPMENT)
+        .antMatchers(HttpMethod.POST,EndpointURI.EQUIPMENT).hasAnyAuthority(PermissionConstants.CREATE_EQUIPMENT)
+        .antMatchers(HttpMethod.PUT,EndpointURI.EQUIPMENT).hasAnyAuthority(PermissionConstants.EDIT_EQUIPMENT)
+        .antMatchers(HttpMethod.DELETE,EndpointURI.DELETE_EQUIPMENT).hasAnyAuthority(PermissionConstants.DELETE_EQUIPMENT);
     
     
     // .anyRequest().authenticated();
--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/util/privilege/PermissionConstants.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/util/privilege/PermissionConstants.java
@@ -13,4 +13,21 @@ public final class PermissionConstants {
   public static final String CREATE_MATERIAL_STATE = "create_material_state";
   public static final String EDIT_MATERIAL_STATE = "edit_material_state";
   public static final String DELETE_MATERIAL_STATE = "delete_material_state";
+  
+  /*
+  raw_material
+  */
+  public static final String VIEW_RAW_MATERIAL = "view_raw_material";
+  public static final String CREATE_RAW_MATERIAL = "create_raw_material";
+  public static final String EDIT_RAW_MATERIAL = "edit_raw_material";
+  public static final String DELETE_RAW_MATERIAL = "delete_raw_material";
+  
+  /*
+  equipment
+  */
+  public static final String VIEW_EQUIPMENT = "view_equipment";
+  public static final String CREATE_EQUIPMENT = "create_equipment";
+  public static final String EDIT_EQUIPMENT = "edit_raw_equipment";
+  public static final String DELETE_EQUIPMENT = "delete_equipment";
+ 
 }
