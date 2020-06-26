a975054d481aa27089fc0b8eab658cf5057712c4
unknown <sujanthan006@gmail.com>
2020-06-26 19:06:54 +0530

    service

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/util/privilege/PermissionConstants.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/util/privilege/PermissionConstants.java
@@ -1,22 +1,22 @@
 package com.tokyo.supermix.util.privilege;
 
 public final class PermissionConstants {
-//  plant
+  //  plant
   public static final String VIEW_PLANT = "view_plant";
   public static final String CREATE_PLANT = "create_plant";
   public static final String EDIT_PLANT = "edit_plant";
   public static final String DELETE_PLANT = "delete_plant";
-// congrete mixer
+  // congrete mixer
   public static final String VIEW_CONCRETE_MIXER = "view_concrete_mixer";
   public static final String CREATE_CONCRETE_MIXER = "create_concrete_mixer";
   public static final String EDIT_CONCRETE_MIXER = "edit_concrete_mixer";
   public static final String DELETE_CONCRETE_MIXER = "delete_concrete_mixer";
-//designation
+ //designation
  public static final String VIEW_DESIGNATION = "view_designation";
  public static final String CREATE_DESIGNATION = "create_designation";
  public static final String EDIT_DESIGNATION = "edit_designation";
  public static final String DELETE_DESIGNATION = "delete_designation";
-// employee
+ // employee
  public static final String VIEW_EMPLOYEE = "view_employee";
  public static final String CREATE_EMPLOYEE = "create_employee";
  public static final String EDIT_EMPLOYEE = "edit_employee";
