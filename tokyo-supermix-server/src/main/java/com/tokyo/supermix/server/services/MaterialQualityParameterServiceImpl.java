5807274c56fb1bcc9171edc659f38628556088cc
kirikaran <kirikarankiri63@gmail.com>
2020-06-27 14:12:23 +0530

    material-quality-parameter api

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/MaterialQualityParameterServiceImpl.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/MaterialQualityParameterServiceImpl.java
@@ -3,20 +3,25 @@ package com.tokyo.supermix.server.services;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+
 import com.tokyo.supermix.data.entities.MaterialQualityParameter;
 import com.tokyo.supermix.data.repositories.MaterialQualityParameterRepository;
+
 
 @Service
 public class MaterialQualityParameterServiceImpl implements MaterialQualityParameterService {
   @Autowired
   private MaterialQualityParameterRepository materialQualityParameterRepository;
 
-  @Override
+  @Transactional
   public void saveMaterialQualityParameter(MaterialQualityParameter materialQualityParameter) {
     materialQualityParameterRepository.save(materialQualityParameter);
   }
 
-  @Override
+  @Transactional
   public boolean isDuplicateRowExists(Long qualityParameterId, Long rawMaterialId, Double value,
       Long unitId) {
     if (materialQualityParameterRepository
@@ -27,24 +32,36 @@ public class MaterialQualityParameterServiceImpl implements MaterialQualityParam
     return false;
   }
 
-  @Override
+  @Transactional(readOnly = true)
   public List<MaterialQualityParameter> getAllMaterialQualityParameters() {
     return materialQualityParameterRepository.findAll();
   }
 
-  @Override
+  @Transactional(readOnly = true)
   public boolean isMaterialQualityParameterIdExist(Long id) {
     return materialQualityParameterRepository.existsById(id);
   }
 
-  @Override
+  @Transactional(propagation = Propagation.NEVER)
   public void deleteMaterialQualityParameter(Long id) {
     materialQualityParameterRepository.deleteById(id);
   }
 
-  @Override
+  @Transactional(readOnly = true)
   public MaterialQualityParameter getMaterialQualityParameterById(Long id) {
     return materialQualityParameterRepository.findById(id).get();
   }
 
+  @Transactional(readOnly = true)
+  public List<MaterialQualityParameter> getAllMaterialQualityParameterByMaterial(
+      Long rawMaterialId) {
+    return materialQualityParameterRepository.findByRawMaterialId(rawMaterialId);
+  }
+
+  @Transactional(readOnly = true)
+  public boolean isMaterialIdExists(Long rawMaterialId) {
+    return materialQualityParameterRepository.existsByRawMaterialId(rawMaterialId);
+  }
 }
+
+
