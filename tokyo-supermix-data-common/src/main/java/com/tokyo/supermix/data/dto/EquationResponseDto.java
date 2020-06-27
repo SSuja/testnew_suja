04c425c301a3fe8fc1be89391c447015fc6eadf7
Mathuragini <mathusharma15@gmail.com>
2020-06-27 15:55:11 +0530

    equation-name-changed

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/dto/EquationResponseDto.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/dto/EquationResponseDto.java
@@ -1,51 +1,52 @@
 package com.tokyo.supermix.data.dto;
 
+import com.tokyo.supermix.data.enums.EquationName;
 import com.tokyo.supermix.data.enums.EquationType;
 
 public class EquationResponseDto {
-  private Long id;
-  private String formula;
-  private String name;
-  private EquationType equationType;
-  private boolean parameterExists;
+	private Long id;
+	private String formula;
+	private EquationName name;
+	private EquationType equationType;
+	private boolean parameterExists;
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public String getFormula() {
-    return formula;
-  }
+	public String getFormula() {
+		return formula;
+	}
 
-  public void setFormula(String formula) {
-    this.formula = formula;
-  }
+	public void setFormula(String formula) {
+		this.formula = formula;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public EquationName getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(EquationName name) {
+		this.name = name;
+	}
 
-  public EquationType getEquationType() {
-    return equationType;
-  }
+	public EquationType getEquationType() {
+		return equationType;
+	}
 
-  public void setEquationType(EquationType equationType) {
-    this.equationType = equationType;
-  }
+	public void setEquationType(EquationType equationType) {
+		this.equationType = equationType;
+	}
 
-  public boolean isParameterExists() {
-    return parameterExists;
-  }
+	public boolean isParameterExists() {
+		return parameterExists;
+	}
 
-  public void setParameterExists(boolean parameterExists) {
-    this.parameterExists = parameterExists;
-  }
+	public void setParameterExists(boolean parameterExists) {
+		this.parameterExists = parameterExists;
+	}
 }
