8cf3996742002d1fb0578dbf82c609104174c004
ramz7 <ramminarayanan7@gmail.com>
2020-06-29 14:01:02 +0530

    email group entities

---
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/EmailRecipient.java
@@ -0,0 +1,65 @@
+package com.tokyo.supermix.data.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import com.tokyo.supermix.data.entities.auth.User;
+import com.tokyo.supermix.data.entities.privilege.PlantRole;
+
+@Entity
+@Table(schema = "tokyo-supermix", name = "email_recipient")
+public class EmailRecipient {
+  private static final long serialVersionUID = 1L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  @ManyToOne
+  @JoinColumn(name = "emailGroupId", nullable = false)
+  private EmailGroup emailGroup;
+  @ManyToOne
+  @JoinColumn(name = "plantRoleId", nullable = true)
+  private PlantRole plantRole;
+  @ManyToOne
+  @JoinColumn(name = "userId", nullable = true)
+  private User user;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public EmailGroup getEmailGroup() {
+    return emailGroup;
+  }
+
+  public void setEmailGroup(EmailGroup emailGroup) {
+    this.emailGroup = emailGroup;
+  }
+
+  public PlantRole getPlantRole() {
+    return plantRole;
+  }
+
+  public void setPlantRole(PlantRole plantRole) {
+    this.plantRole = plantRole;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public static long getSerialversionuid() {
+    return serialVersionUID;
+  }
+}
