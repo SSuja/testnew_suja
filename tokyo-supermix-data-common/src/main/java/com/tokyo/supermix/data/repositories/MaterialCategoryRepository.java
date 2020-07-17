aa087b12f3de5a00bc668ee557ffbb9afb76c2b3
geerthana65 <geerthana65@gmail.com>
2020-07-17 15:22:04 +0530

    get material category by main type

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/MaterialCategoryRepository.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/MaterialCategoryRepository.java
@@ -1,12 +1,18 @@
 package com.tokyo.supermix.data.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.tokyo.supermix.data.entities.MaterialCategory;
+import com.tokyo.supermix.data.enums.MainType;
 
 @Repository
 public interface MaterialCategoryRepository extends JpaRepository<MaterialCategory, Long> {
   boolean existsByName(String name);
 
   MaterialCategory findByName(String name);
+
+  List<MaterialCategory> findByMainType(MainType mainType);
+
+  boolean existsByMainType(MainType mainType);
 }
