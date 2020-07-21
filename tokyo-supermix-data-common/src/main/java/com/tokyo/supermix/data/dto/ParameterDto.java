f90167b0efad68d95321d2b495f39a6913162edf
Mathuragini <mathuthiyasharma@gmail.com>
2020-07-21 10:32:27 +0530

    review-fixed

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/dto/ParameterDto.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/dto/ParameterDto.java
@@ -5,33 +5,33 @@ import javax.validation.constraints.NotNull;
 import com.tokyo.supermix.data.enums.ParameterType;
 
 public class ParameterDto {
-  private Long id;
-  @NotNull(message = "{parameterDto.name.null}")
-  @NotEmpty(message = "{parameterDto.name.empty}")
-  private String name;
-  private ParameterType parameterType;
+	private Long id;
+	@NotNull(message = "{parameterDto.name.null}")
+	@NotEmpty(message = "{parameterDto.name.empty}")
+	private String name;
+	private ParameterType parameterType;
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public ParameterType getParameterType() {
-    return parameterType;
-  }
+	public ParameterType getParameterType() {
+		return parameterType;
+	}
 
-  public void setParameterType(ParameterType parameterType) {
-    this.parameterType = parameterType;
-  }
+	public void setParameterType(ParameterType parameterType) {
+		this.parameterType = parameterType;
+	}
 }
--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/Parameter.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/Parameter.java
@@ -1,7 +1,6 @@
 package com.tokyo.supermix.data.entities;
 
 import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -15,39 +14,39 @@ import com.tokyo.supermix.data.enums.ParameterType;
 @Entity
 @Table(schema = "tokyo-supermix", name = "parameter")
 public class Parameter implements Serializable {
-  private static final long serialVersionUID = 1L;
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
-  private String name;
-  @Enumerated(EnumType.ORDINAL)
-  private ParameterType parameterType;
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	@Enumerated(EnumType.ORDINAL)
+	private ParameterType parameterType;
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public ParameterType getParameterType() {
-    return parameterType;
-  }
+	public ParameterType getParameterType() {
+		return parameterType;
+	}
 
-  public void setParameterType(ParameterType parameterType) {
-    this.parameterType = parameterType;
-  }
+	public void setParameterType(ParameterType parameterType) {
+		this.parameterType = parameterType;
+	}
 
-  public static long getSerialversionuid() {
-    return serialVersionUID;
-  }
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 }
