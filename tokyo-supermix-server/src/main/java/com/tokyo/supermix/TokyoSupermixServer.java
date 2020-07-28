c96cf11dfa4fbe88189edea18c66e5d6879a0d41
unknown <sujanthan006@gmail.com>
2020-07-28 13:14:53 +0530

    body

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/TokyoSupermixServer.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/TokyoSupermixServer.java
@@ -1,15 +1,18 @@
 package com.tokyo.supermix;
 
+import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class TokyoSupermixServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(TokyoSupermixServer.class, args);
+           SpringApplication.run(TokyoSupermixServer.class, args);
     }
 
 }
