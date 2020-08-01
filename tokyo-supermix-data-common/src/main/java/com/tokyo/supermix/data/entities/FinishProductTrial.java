f449a3deca989cb2ee6a9a9a956e7a7030d5bb89
Mathuragini <mathuthiyasharma@gmail.com>
2020-08-01 15:55:54 +0530

    error-fixed

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/FinishProductTrial.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/entities/FinishProductTrial.java
@@ -3,6 +3,8 @@ package com.tokyo.supermix.data.entities;
 import java.io.Serializable;
 import java.sql.Date;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
