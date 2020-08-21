275310258e11835c7f05960cb99760470757628b
ramz7 <ramminarayanan7@gmail.com>
2020-08-21 11:27:29 +0530

    added email configuration to properties file

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/util/MailConstants.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/util/MailConstants.java
@@ -1,44 +1,32 @@
 package com.tokyo.supermix.util;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
 
 /**
  * contains custom Email Address
  *
  */
 
-@Component
-@PropertySource("classpath:mail.properties")
 public class MailConstants {
-	@Value("${mail.notify.admin}")
-	private String admin;
+  @Value("${mail.notificationTime.plantEquipment}")
+  private String plantEquipmentTime;
+  @Value("${mail.notificationTime.strengthTestMixDesign}")
+  private String strengthTestMixDesign;
 
-	@Value("${mail.notify.report}")
-    private String[] mailReportUser;
-	@Value("${mail.notificationTime.plantEquipment}")
-    private String plantEquipmentTime;
-	@Value("${mail.notificationTime.strengthTestMixDesign}")
-    private String strengthTestMixDesign;
-	
-	public String getAdmin() {
-		return admin;
-	}
-	public void setAdmin(String admin) {
-		this.admin = admin;
-	}
+  public String getPlantEquipmentTime() {
+    return plantEquipmentTime;
+  }
 
-    public String[] getMailReportUser() {
-      return mailReportUser;
-    }
-    public void setMailReportUser(String[] mailReportUser) {
-      this.mailReportUser = mailReportUser;
-    }
-    public String getPlantEquipmentTime() {
-      return plantEquipmentTime;
-    }
-    public void setPlantEquipmentTime(String plantEquipmentTime) {
-      this.plantEquipmentTime = plantEquipmentTime;
-    }    
+  public void setPlantEquipmentTime(String plantEquipmentTime) {
+    this.plantEquipmentTime = plantEquipmentTime;
+  }
+
+  public String getStrengthTestMixDesign() {
+    return strengthTestMixDesign;
+  }
+
+  public void setStrengthTestMixDesign(String strengthTestMixDesign) {
+    this.strengthTestMixDesign = strengthTestMixDesign;
+  }
+  
 }
