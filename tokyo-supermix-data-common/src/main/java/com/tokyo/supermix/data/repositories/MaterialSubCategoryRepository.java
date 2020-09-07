7775331c6ec1305a258b87da1636c80dc6888155
Mathuragini <mathuthiyasharma@gmail.com>
2020-09-07 13:06:46 +0530

    material-sub-category-modified

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/MaterialSubCategoryRepository.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/MaterialSubCategoryRepository.java
@@ -1,13 +1,17 @@
 package com.tokyo.supermix.data.repositories;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import com.tokyo.supermix.data.entities.MaterialCategory;
 import com.tokyo.supermix.data.entities.MaterialSubCategory;
 
 public interface MaterialSubCategoryRepository extends JpaRepository<MaterialSubCategory, Long>,
-    QuerydslPredicateExecutor<MaterialSubCategory> {
+    QuerydslPredicateExecutor<MaterialSubCategory>,
+    PagingAndSortingRepository<MaterialSubCategory, Long> {
   boolean existsByName(String name);
 
   List<MaterialSubCategory> findByMaterialCategory(MaterialCategory materialCategory);
@@ -19,4 +23,6 @@ public interface MaterialSubCategoryRepository extends JpaRepository<MaterialSub
   List<MaterialSubCategory> findByMaterialCategoryId(Long materialCategoryId);
 
   boolean existsByNameAndMaterialCategoryId(String name, Long materialCategoryId);
+
+  Page<MaterialSubCategory> findAll(Pageable pageable);
 }
--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/MaterialSubCategoryService.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/MaterialSubCategoryService.java
@@ -2,35 +2,37 @@ package com.tokyo.supermix.server.services;
 
 import java.util.List;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import com.querydsl.core.types.Predicate;
 import com.tokyo.supermix.data.entities.MaterialCategory;
 import com.tokyo.supermix.data.entities.MaterialSubCategory;
 
 public interface MaterialSubCategoryService {
-  public List<MaterialSubCategory> getMaterialSubCategories();
+	public List<MaterialSubCategory> getMaterialSubCategories();
 
-  public MaterialSubCategory getMaterialSubCategoryById(long id);
+	public MaterialSubCategory getMaterialSubCategoryById(long id);
 
-  public void deleteMaterialSubCategory(Long id);
+	public void deleteMaterialSubCategory(Long id);
 
-  public void saveMaterialSubCategory(MaterialSubCategory materialSubCategory);
+	public void saveMaterialSubCategory(MaterialSubCategory materialSubCategory);
 
-  public List<MaterialSubCategory> getMaterialSubCategoryByCategory(
-      MaterialCategory materialCategory);
+	public List<MaterialSubCategory> getMaterialSubCategoryByCategory(MaterialCategory materialCategory);
 
-  public boolean isMaterialSubCategoryExist(Long id);
+	public boolean isMaterialSubCategoryExist(Long id);
 
-  public boolean isMaterialSubCategoryNameExist(String name);
+	public boolean isMaterialSubCategoryNameExist(String name);
 
-  public boolean isUpdatedMaterialSubCategoryNameExist(Long id, String name,
-      Long materialCategoryId);
+	public boolean isUpdatedMaterialSubCategoryNameExist(Long id, String name, Long materialCategoryId);
 
-  public boolean isMaterialCategoryIdExist(Long materialCategoryId);
+	public boolean isMaterialCategoryIdExist(Long materialCategoryId);
 
-  public MaterialSubCategory getMaterialSubCategoryByName(String name);
+	public MaterialSubCategory getMaterialSubCategoryByName(String name);
 
-  public Page<MaterialSubCategory> searchMaterialSubCategory(Predicate predicate, int size,
-      int page);
+	public Page<MaterialSubCategory> searchMaterialSubCategory(Predicate predicate, int size, int page);
 
-  public boolean isMaterialCategoryExist(String name, Long materialCategoryId);
+	public boolean isMaterialCategoryExist(String name, Long materialCategoryId);
+
+	public List<MaterialSubCategory> getAllMaterialSubCategories(Pageable pageable);
+
+	public Long getCountMaterialSubCategory();
 }
