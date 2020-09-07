5ea88fbb4358bebb60443792a3bfe1000a267bc3
kirikaran <kirikarankiri63@gmail.com>
2020-09-07 17:38:52 +0530

    finish-product-sample-issue-pagination

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/FinishProductSampleIssueRepository.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/FinishProductSampleIssueRepository.java
@@ -1,16 +1,23 @@
 package com.tokyo.supermix.data.repositories;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import com.tokyo.supermix.data.entities.FinishProductSampleIssue;
 
 public interface FinishProductSampleIssueRepository
     extends JpaRepository<FinishProductSampleIssue, String>,
-    QuerydslPredicateExecutor<FinishProductSampleIssue> {
-  List<FinishProductSampleIssue> findByMixDesignPlantCode(String plantCode);
+    QuerydslPredicateExecutor<FinishProductSampleIssue>,
+    PagingAndSortingRepository<FinishProductSampleIssue, String> {
+  Page<FinishProductSampleIssue> findByMixDesignPlantCode(String plantCode, Pageable pageable);
 
-  List<FinishProductSampleIssue> findByProjectPlantCodeIn(List<String> plantCodes);
+  Page<FinishProductSampleIssue> findByProjectPlantCodeIn(List<String> plantCodes,
+      Pageable pageable);
 
   List<FinishProductSampleIssue> findByCodeContaining(String code);
+
+  Long countByMixDesignPlantCode(String plantCode);
 }
