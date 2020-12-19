115cb1483612af4afd4bbb3066c9e7b6f3d12f63
Mathuragini <mathuthiyasharma@gmail.com>
2020-12-19 11:37:34 +0530

    ratio-config-repo

---
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/RatioConfigEquationRepository.java
@@ -0,0 +1,8 @@
+package com.tokyo.supermix.data.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tokyo.supermix.data.entities.RatioConfigEquation;
+
+public interface RatioConfigEquationRepository extends JpaRepository<RatioConfigEquation, Long> {
+
+}
