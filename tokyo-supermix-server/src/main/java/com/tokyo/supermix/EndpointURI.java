5acd4288c637ca1a7ebf870fd07a48f9c9f9d875
Kirikaran <kirikarankiri63@gmail.com>
2020-12-20 20:20:54 +0530

    materialTest

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/EndpointURI.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/EndpointURI.java
@@ -732,6 +732,10 @@ public final class EndpointURI {
       BASE_API_PATH + SLASH + "finished-product-delivery" + SLASH + "download";
   public static final String IMPORT_FINISHED_PRODUCT_DELIVERY =
       BASE_API_PATH + SLASH + "finished-product-delivery" + SLASH + "upload";
+  
+  
+  public static final String GET_MATERIAL_TESTS_BY_INCOMING_SAMPLE =
+      MATERIAL_TEST + SLASH + "incoming-sample" + SLASH + "{incomingSampleCode}";
 
   private EndpointURI() {}
 }
