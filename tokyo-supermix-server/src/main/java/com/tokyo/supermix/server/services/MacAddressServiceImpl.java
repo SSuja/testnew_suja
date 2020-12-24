023ede2c35d30940b221d93ee4539b646aed3d73
Your Name <sujanthan006@gmail.com>
2020-12-24 03:44:54 -0800

    windows ubunthu

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/MacAddressServiceImpl.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/MacAddressServiceImpl.java
@@ -6,23 +6,18 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Enumeration;
 import java.util.List;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import com.tokyo.supermix.data.entities.MacAddress;
 import com.tokyo.supermix.data.repositories.MacAddressRepository;
-import com.tokyo.supermix.server.controller.EmployeeController;
-
-
 
 @Service
 public class MacAddressServiceImpl implements MacAddressService {
 
   @Autowired
   MacAddressRepository macAddressRepository;
-  private static final Logger logger = Logger.getLogger(EmployeeController.class);
 
   @Transactional
   public void saveMacAddress(MacAddress macAddress) {
@@ -52,25 +47,43 @@ public class MacAddressServiceImpl implements MacAddressService {
   public String getClientMACAddress() {
     InetAddress ip;
     StringBuilder sb = new StringBuilder();
-    try {
-      Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-      while (networkInterfaces.hasMoreElements()) {
-        NetworkInterface network = networkInterfaces.nextElement();
-        byte[] mac = network.getHardwareAddress();
-        if (mac == null) {
-          logger.debug(getClientMACAddress(), null);
-        } else {
+
+    if (System.getProperty("os.name").equalsIgnoreCase("Linux")) {
+      try {
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        while (networkInterfaces.hasMoreElements()) {
+          NetworkInterface network = networkInterfaces.nextElement();
+          byte[] mac = network.getHardwareAddress();
+          if (mac == null) {
+            System.out.println("MAc");
+          } else {
             for (int i = 0; i < mac.length; i++) {
-            sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+              sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? ("-") : ""));
+            }
+            break;
           }
-          break;
         }
+        ip = InetAddress.getLocalHost();
+      } catch (UnknownHostException e) {
+        e.printStackTrace();
+      } catch (SocketException e) {
+        e.printStackTrace();
       }
-      ip = InetAddress.getLocalHost();
-    } catch (UnknownHostException e) {
-      e.printStackTrace();
-    } catch (SocketException e) {
-      e.printStackTrace();
+    } else {
+      try {
+
+        ip = InetAddress.getLocalHost();
+        NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+        byte[] mac = network.getHardwareAddress();
+
+        for (int i = 0; i < mac.length; i++) {
+          sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+        }
+      } catch (UnknownHostException e) {
+        e.printStackTrace();
+      } catch (SocketException e) {
+        e.printStackTrace();
+      }
     }
     return sb.toString();
   }
