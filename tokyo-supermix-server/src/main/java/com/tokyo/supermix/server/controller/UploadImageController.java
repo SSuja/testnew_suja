ac2ae59e687cceb20b061e14178e7951c7296110
Mathuragini <mathuthiyasharma@gmail.com>
2021-01-04 09:11:01 +0530

    file empty validation added

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/UploadImageController.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/UploadImageController.java
@@ -56,6 +56,10 @@ public class UploadImageController {
       return new ResponseEntity<>(new ValidationFailureResponse(Constants.UPLOAD_IMAGE,
           validationFailureStatusCodes.getTestIdNull()), HttpStatus.BAD_REQUEST);
     }
+    if (Arrays.asList(file).isEmpty()) {
+      return new ResponseEntity<>(new ValidationFailureResponse(Constants.UPLOAD_IMAGE,
+          validationFailureStatusCodes.getTestIdNull()), HttpStatus.BAD_REQUEST);
+    }
     List<String> testImage = new ArrayList<>();
     Arrays.asList(file).stream().forEach(files -> {
       try {
