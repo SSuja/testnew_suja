c75abe928dae94f1265be3ae7185396d645608a2
Mathuragini <mathuthiyasharma@gmail.com>
2021-01-18 15:50:46 +0530

    files-added-for-multi result formula

---
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/repositories/MultiResultFormulaRepository.java
@@ -0,0 +1,8 @@
+package com.tokyo.supermix.data.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tokyo.supermix.data.entities.MultiResultFormula;
+
+public interface MultiResultFormulaRepository extends JpaRepository<MultiResultFormula, Long> {
+
+}
