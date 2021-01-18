fda54b1324c23bf661120431f5541cb609269ac6
Keerthana65 <geerthana65@gmail.com>
2021-01-18 17:13:05 +0530

    get multi result formula by test configure id

--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/MultiResultFormulaController.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/controller/MultiResultFormulaController.java
@@ -1,5 +1,42 @@
 package com.tokyo.supermix.server.controller;
 
-public class MultiResultFormulaController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import com.tokyo.supermix.EndpointURI;
+import com.tokyo.supermix.data.dto.MultiResultFormulaResponseDto;
+import com.tokyo.supermix.data.mapper.Mapper;
+import com.tokyo.supermix.rest.enums.RestApiResponseStatus;
+import com.tokyo.supermix.rest.response.ContentResponse;
+import com.tokyo.supermix.rest.response.ValidationFailureResponse;
+import com.tokyo.supermix.server.services.MultiResultFormulaService;
+import com.tokyo.supermix.util.Constants;
+import com.tokyo.supermix.util.ValidationFailureStatusCodes;
 
+@RestController
+@CrossOrigin(origins = "*")
+public class MultiResultFormulaController {
+  @Autowired
+  private MultiResultFormulaService multiResultFormulaService;
+  @Autowired
+  private ValidationFailureStatusCodes validationFailureStatusCodes;
+  @Autowired
+  private Mapper mapper;
+
+  @GetMapping(value = EndpointURI.MULTI_RESULT_FORMULA_BY_TEST_CONFIGURE_ID)
+  public ResponseEntity<Object> getMultiResultFormulaByTestConfigureId(
+      @PathVariable Long testConfigureId) {
+    if (multiResultFormulaService.isExistsTestConfigureId(testConfigureId)) {
+      return new ResponseEntity<>(new ContentResponse<>(Constants.MULTI_RESULT_FORMULA,
+          mapper.map(multiResultFormulaService.getByTestConfigureId(testConfigureId),
+              MultiResultFormulaResponseDto.class),
+          RestApiResponseStatus.OK), HttpStatus.OK);
+    }
+    return new ResponseEntity<>(new ValidationFailureResponse(Constants.TEST_CONFIGURE_ID,
+        validationFailureStatusCodes.getTestConfigureNotExist()), HttpStatus.BAD_REQUEST);
+  }
 }
--- tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/MultiResultFormulaServiceImpl.java
+++ tokyo-supermix-server/src/main/java/com/tokyo/supermix/server/services/MultiResultFormulaServiceImpl.java
@@ -1,8 +1,24 @@
 package com.tokyo.supermix.server.services;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.tokyo.supermix.data.entities.MultiResultFormula;
+import com.tokyo.supermix.data.repositories.MultiResultFormulaRepository;
 
 @Service
-public class MultiResultFormulaServiceImpl {
+public class MultiResultFormulaServiceImpl implements MultiResultFormulaService {
+  @Autowired
+  private MultiResultFormulaRepository multiResultFormulaRepository;
 
+  @Transactional(readOnly = true)
+  public List<MultiResultFormula> getByTestConfigureId(Long testConfigureId) {
+    return multiResultFormulaRepository.findByTestConfigureId(testConfigureId);
+  }
+
+  @Transactional(readOnly = true)
+  public boolean isExistsTestConfigureId(Long testConfigureId) {
+    return multiResultFormulaRepository.existsByTestConfigureId(testConfigureId);
+  }
 }
