0cca099ea4de276d615323357a4371d97df5f4a2
Mathuragini <mathusharma15@gmail.com>
2021-02-16 14:31:01 +0530

    add result type in testconfig, update reset delete

--- tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/dto/TestConfigureResDto.java
+++ tokyo-supermix-data-common/src/main/java/com/tokyo/supermix/data/dto/TestConfigureResDto.java
@@ -1,108 +1,117 @@
 package com.tokyo.supermix.data.dto;
 
 import javax.annotation.Nullable;
+import com.tokyo.supermix.data.enums.TestResultType;
 
 public class TestConfigureResDto {
-	private Long testConfigureId;
-	private Long materialSubCategoryId;
-	private String materialSubCategoryName;
-	private String testName;
-	private Long materialCategoryId;
-	private String materialCategoryName;
-	@Nullable
-	private Long rawMaterialId;
-	@Nullable
-	private String rawMaterialName;
-	private String acceptedType;
-	private String testType;
-	private String dueDay;
+  private Long testConfigureId;
+  private Long materialSubCategoryId;
+  private String materialSubCategoryName;
+  private String testName;
+  private Long materialCategoryId;
+  private String materialCategoryName;
+  @Nullable
+  private Long rawMaterialId;
+  @Nullable
+  private String rawMaterialName;
+  private String acceptedType;
+  private String testType;
+  private String dueDay;
+  private TestResultType testResultType;
 
-	public Long getTestConfigureId() {
-		return testConfigureId;
-	}
+  public Long getTestConfigureId() {
+    return testConfigureId;
+  }
 
-	public void setTestConfigureId(Long testConfigureId) {
-		this.testConfigureId = testConfigureId;
-	}
+  public void setTestConfigureId(Long testConfigureId) {
+    this.testConfigureId = testConfigureId;
+  }
 
-	public Long getMaterialSubCategoryId() {
-		return materialSubCategoryId;
-	}
+  public Long getMaterialSubCategoryId() {
+    return materialSubCategoryId;
+  }
 
-	public void setMaterialSubCategoryId(Long materialSubCategoryId) {
-		this.materialSubCategoryId = materialSubCategoryId;
-	}
+  public void setMaterialSubCategoryId(Long materialSubCategoryId) {
+    this.materialSubCategoryId = materialSubCategoryId;
+  }
 
-	public String getMaterialSubCategoryName() {
-		return materialSubCategoryName;
-	}
+  public String getMaterialSubCategoryName() {
+    return materialSubCategoryName;
+  }
 
-	public void setMaterialSubCategoryName(String materialSubCategoryName) {
-		this.materialSubCategoryName = materialSubCategoryName;
-	}
+  public void setMaterialSubCategoryName(String materialSubCategoryName) {
+    this.materialSubCategoryName = materialSubCategoryName;
+  }
 
-	public String getTestName() {
-		return testName;
-	}
+  public String getTestName() {
+    return testName;
+  }
 
-	public void setTestName(String testName) {
-		this.testName = testName;
-	}
+  public void setTestName(String testName) {
+    this.testName = testName;
+  }
 
-	public Long getMaterialCategoryId() {
-		return materialCategoryId;
-	}
+  public Long getMaterialCategoryId() {
+    return materialCategoryId;
+  }
 
-	public void setMaterialCategoryId(Long materialCategoryId) {
-		this.materialCategoryId = materialCategoryId;
-	}
+  public void setMaterialCategoryId(Long materialCategoryId) {
+    this.materialCategoryId = materialCategoryId;
+  }
 
-	public String getMaterialCategoryName() {
-		return materialCategoryName;
-	}
+  public String getMaterialCategoryName() {
+    return materialCategoryName;
+  }
 
-	public void setMaterialCategoryName(String materialCategoryName) {
-		this.materialCategoryName = materialCategoryName;
-	}
+  public void setMaterialCategoryName(String materialCategoryName) {
+    this.materialCategoryName = materialCategoryName;
+  }
 
-	public Long getRawMaterialId() {
-		return rawMaterialId;
-	}
+  public Long getRawMaterialId() {
+    return rawMaterialId;
+  }
 
-	public void setRawMaterialId(Long rawMaterialId) {
-		this.rawMaterialId = rawMaterialId;
-	}
+  public void setRawMaterialId(Long rawMaterialId) {
+    this.rawMaterialId = rawMaterialId;
+  }
 
-	public String getRawMaterialName() {
-		return rawMaterialName;
-	}
+  public String getRawMaterialName() {
+    return rawMaterialName;
+  }
 
-	public void setRawMaterialName(String rawMaterialName) {
-		this.rawMaterialName = rawMaterialName;
-	}
+  public void setRawMaterialName(String rawMaterialName) {
+    this.rawMaterialName = rawMaterialName;
+  }
 
-	public String getAcceptedType() {
-		return acceptedType;
-	}
+  public String getAcceptedType() {
+    return acceptedType;
+  }
 
-	public void setAcceptedType(String acceptedType) {
-		this.acceptedType = acceptedType;
-	}
+  public void setAcceptedType(String acceptedType) {
+    this.acceptedType = acceptedType;
+  }
 
-	public String getTestType() {
-		return testType;
-	}
+  public String getTestType() {
+    return testType;
+  }
 
-	public void setTestType(String testType) {
-		this.testType = testType;
-	}
+  public void setTestType(String testType) {
+    this.testType = testType;
+  }
 
-	public String getDueDay() {
-		return dueDay;
-	}
+  public String getDueDay() {
+    return dueDay;
+  }
 
-	public void setDueDay(String dueDay) {
-		this.dueDay = dueDay;
-	}
+  public void setDueDay(String dueDay) {
+    this.dueDay = dueDay;
+  }
 
+  public TestResultType getTestResultType() {
+    return testResultType;
+  }
+
+  public void setTestResultType(TestResultType testResultType) {
+    this.testResultType = testResultType;
+  }
 }
